7409c2b948cb490ea0761af8d704f28e7774ad8c
Jon Jagger <jon@jaggersoft.com>
2016-07-28 17:36:11 +0100

    add checks for required docker tools to script

--- diff.sh
+++ diff.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-#my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-#docker_compose_cmd="docker-compose --file=${my_dir}/docker-compose.yml"
+exit_if_not_installed() {
+  hash ${1} 2> /dev/null
+  if [ $? != 0 ]; then
+    echo
+    echo "${1} is not installed"
+    exit 1
+  fi
+}
+
+exit_if_not_installed 'docker'
+exit_if_not_installed 'docker-machine'
+exit_if_not_installed 'docker-compose'
 
 cd client && ./build-image.sh
 cd ..
