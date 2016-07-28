ea0f369a55a82284103dfa1788889321124698c9
Jon Jagger <jon@jaggersoft.com>
2016-07-28 14:11:41 +0100

    server now receives request from client

--- differ_client/app.rb
+++ differ_client/app.rb
@@ -1,11 +1,24 @@
 require 'sinatra'
 require 'sinatra/base'
 require 'json'
+require 'net/http'
 
+# DifferClient
 class App < Sinatra::Base
+
   get '/' do
-    'Talk to differ_server by getting ENV'
+    differ_server = ENV['DIFFER_SERVER_PORT']
+    addr = differ_server.sub('tcp', 'http') + '/differ'
+    uri = URI(addr)
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.path, 'Content-Type' => 'application/json')
+
+    req.body = { name: 'John Doe', role: 'agent' }.to_json
+
+    res = http.request(req)
+    res.body
   end
+
 end
 
 
--- differ_server/app.rb
+++ differ_server/app.rb
@@ -2,8 +2,12 @@ require 'sinatra'
 require 'sinatra/base'
 require 'json'
 
+# DifferServer
 class App < Sinatra::Base
-  get '/' do
+  get '/differ' do
+
+    p "/differ: received request..."
+
     content_type :json
     { :key1 => 'Hello from docker!' }.to_json
   end
