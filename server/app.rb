7aac79cb109c97f0ad8794a5d2c9eebb000654a7
Jon Jagger <jon@jaggersoft.com>
2016-07-28 14:48:45 +0100

    refactoring

--- server/app.rb
+++ server/app.rb
@@ -4,13 +4,27 @@ require 'json'
 
 class App < Sinatra::Base
   get '/diff' do
-
-    hash = JSON.parse(request.body.read)
-    p hash
+    p was
+    p now
 
     content_type :json
     { :key => 'was<-diff->now' }.to_json
   end
+
+  private
+
+  def was
+    arg['was']
+  end
+
+  def now
+    arg['now']
+  end
+
+  def arg
+    @arg ||= JSON.parse(request.body.read)
+  end
+
 end
 
 
