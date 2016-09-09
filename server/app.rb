761bc94de435de139f59e3bdb3bc9e7d171bdf46
Jon Jagger <jon@jaggersoft.com>
2016-09-09 11:03:57 +0100

    refactoring

--- server/app.rb
+++ server/app.rb
@@ -27,58 +27,56 @@ class App < Sinatra::Base
   private
 
   def diff
-    Dir.mktmpdir('differ') do |tmp_dir|
-      @git_dir = tmp_dir
-      make_empty_git_repo
-      copy_was_files_into_tag_0
-      add_remove_copy_now_files_into_tag_1
-      diff_lines = git.diff(tmp_dir, 0, 1)
+    Dir.mktmpdir('differ') do |git_dir|
+      make_empty_git_repo_in(git_dir)
+
+      write_files_into(git_dir)
+      git.commit(git_dir, was_tag=0)
+
+      write_new_files_to(git_dir)
+      delete_deleted_files_from(git_dir)
+      overwrite_changed_files_in(git_dir)
+      git.commit(git_dir, now_tag=1)
+
+      diff_lines = git.diff(git_dir, was_tag, now_tag)
       git_diff(diff_lines, now_files)
     end
   end
 
-  def make_empty_git_repo
+  def make_empty_git_repo_in(git_dir)
     user_name = 'nobody'
     user_email = user_name + '@cyber-dojo.org'
     git.setup(git_dir, user_name, user_email)
   end
 
-  def git_dir
-    @git_dir
-  end
-
-  def copy_was_files_into_tag_0
-    was_files.each do |filename,content|
+  def write_files_into(git_dir)
+    was_files.each do |filename, content|
       file.write(git_dir + '/' + filename, content)
       git.add(git_dir, filename)
     end
-    git.commit(git_dir, was_tag=0)
   end
 
-  def add_remove_copy_now_files_into_tag_1
-    delta = make_delta(was_files, now_files)
+  def write_new_files_to(git_dir)
     delta[:new].each do |filename|
       file.write(git_dir + '/' + filename, now_files[filename])
       git.add(git_dir, filename)
     end
+  end
+
+  def delete_deleted_files_from(git_dir)
     delta[:deleted].each do |filename|
       git.rm(git_dir, filename)
     end
+  end
+
+  def overwrite_changed_files_in(git_dir)
     delta[:changed].each do |filename|
       file.write(git_dir + '/' + filename, now_files[filename])
     end
-    git.commit(git_dir, now_tag=1)
   end
 
-  def log;   @log   ||= external_object; end
-  def shell; @shell ||= external_object; end
-  def git;   @git   ||= external_object; end
-  def file;  @file  ||= external_object; end
-
-  def external_object
-    name = 'DIFFER_CLASS_' + name_of(caller).upcase
-    var = unslashed(ENV[name] || fail("ENV[#{name}] not set"))
-    Object.const_get(var).new(self)
+  def delta
+    make_delta(was_files, now_files)
   end
 
   def was_files
@@ -91,6 +89,17 @@ class App < Sinatra::Base
 
   def arg
     @arg ||= JSON.parse(request.body.read)
+  end
+
+  def log;   @log   ||= external_object; end
+  def shell; @shell ||= external_object; end
+  def git;   @git   ||= external_object; end
+  def file;  @file  ||= external_object; end
+
+  def external_object
+    name = 'DIFFER_CLASS_' + name_of(caller).upcase
+    var = unslashed(ENV[name] || fail("ENV[#{name}] not set"))
+    Object.const_get(var).new(self)
   end
 
   include NameOfCaller
