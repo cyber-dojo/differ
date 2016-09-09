d3170f52f5e5786d4966ee2e06d71c8b73d633ad
Jon Jagger <jon@jaggersoft.com>
2016-09-09 10:30:01 +0100

    refactoring

--- server/app.rb
+++ server/app.rb
@@ -20,53 +20,61 @@ require 'json'
 class App < Sinatra::Base
 
   get '/diff' do
-    diff = nil
-    Dir.mktmpdir('differ') do |tmp_dir|
-      # make empty git repo
-      user_name = 'differ'
-      user_email = user_name + '@cyber-dojo.org'
-      git.setup(tmp_dir, user_name, user_email)
-
-      # create sandbox subdir so I don't need to alter parser
-      sandbox_dir = tmp_dir + '/' + 'sandbox'
-      shell.exec("mkdir #{sandbox_dir}")
-
-      # copy was_files into tag 0
-      was_files.each do |filename,content|
-        file.write(sandbox_dir + '/' + filename, content)
-        git.add(sandbox_dir, filename)
-      end
-      git.commit(tmp_dir, was_tag=0)
-
-      # add/remove/copy now_files into tag 1
-      delta = make_delta(was_files, now_files)
-      delta[:new].each do |filename|
-        file.write(sandbox_dir + '/' + filename, now_files[filename])
-        git.add(sandbox_dir, filename)
-      end
-      delta[:deleted].each do |filename|
-        git.rm(sandbox_dir, filename)
-      end
-      delta[:changed].each do |filename|
-        file.write(sandbox_dir + '/' + filename, now_files[filename])
-      end
-      git.commit(tmp_dir, now_tag=1)
-
-      # get the diff
-      diff_lines = git.diff(tmp_dir, was_tag, now_tag)
-      diff = git_diff(diff_lines, now_files)
-    end
-
     content_type :json
     diff.to_json
   end
 
   private
 
-  include NameOfCaller
-  include Unslashed
-  include DeltaMaker
-  include GitDiff
+  def diff
+    Dir.mktmpdir('differ') do |tmp_dir|
+      @tmp_dir = tmp_dir
+      make_empty_git_repo
+      create_sandbox_dir
+      copy_was_files_into_tag_0
+      add_remove_copy_now_files_into_tag_1
+      diff_lines = git.diff(tmp_dir, 0, 1)
+      git_diff(diff_lines, now_files)
+    end
+  end
+
+  def make_empty_git_repo
+    user_name = 'nobody'
+    user_email = user_name + '@cyber-dojo.org'
+    git.setup(@tmp_dir, user_name, user_email)
+  end
+
+  def sandbox_dir
+    @tmp_dir + '/' + 'sandbox'
+  end
+
+  def create_sandbox_dir
+    # so I don't need to alter parser
+    shell.exec("mkdir #{sandbox_dir}")
+  end
+
+  def copy_was_files_into_tag_0
+    was_files.each do |filename,content|
+      file.write(sandbox_dir + '/' + filename, content)
+      git.add(sandbox_dir, filename)
+    end
+    git.commit(@tmp_dir, was_tag=0)
+  end
+
+  def add_remove_copy_now_files_into_tag_1
+    delta = make_delta(was_files, now_files)
+    delta[:new].each do |filename|
+      file.write(sandbox_dir + '/' + filename, now_files[filename])
+      git.add(sandbox_dir, filename)
+    end
+    delta[:deleted].each do |filename|
+      git.rm(sandbox_dir, filename)
+    end
+    delta[:changed].each do |filename|
+      file.write(sandbox_dir + '/' + filename, now_files[filename])
+    end
+    git.commit(@tmp_dir, now_tag=1)
+  end
 
   def log;   @log   ||= external_object; end
   def shell; @shell ||= external_object; end
@@ -90,6 +98,11 @@ class App < Sinatra::Base
   def arg
     @arg ||= JSON.parse(request.body.read)
   end
+
+  include NameOfCaller
+  include Unslashed
+  include DeltaMaker
+  include GitDiff
 
 end
 
