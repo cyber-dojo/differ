7d311132cc0dd28b9b91cbab7ed5a71dfc344a13
Jon Jagger <jon@jaggersoft.com>
2016-09-09 16:19:27 +0100

    delete dangling images in test and up scripts

--- test.sh
+++ test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+docker images --quiet --filter=dangling=true | xargs docker rmi
 ./build.sh && docker run --rm cyberdojo/differ sh -c "cd test/lib && ./run.sh"
--- up.sh
+++ up.sh
@@ -5,6 +5,8 @@
 ip=$(docker-machine ip default)
 echo "${ip}:4568/diff"
 
+docker images --quiet --filter=dangling=true | xargs docker rmi
+
 docker-compose up &
 
 
