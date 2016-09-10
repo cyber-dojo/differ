dabd89818beb176f9fa1e43d379136ecb721d330
Jon Jagger <jon@jaggersoft.com>
2016-09-10 09:18:40 +0100

    refactoring; simpler differ implementation

--- server/lib/differ.rb
+++ server/lib/differ.rb
@@ -6,24 +6,23 @@ require_relative './git_diff'
 class Differ
 
   def initialize(was_files, now_files)
-    @was_files = was_files
-    @now_files = now_files
     @delta = make_delta(was_files, now_files)
+    @now_files = now_files
   end
-
-  attr_reader :was_files, :now_files
 
   def diff
     Dir.mktmpdir('differ') do |git_dir|
       make_empty_git_repo_in(git_dir)
 
+      was_tag = 0
       write_files_into(git_dir)
-      git.commit(git_dir, was_tag=0)
+      git.commit(git_dir, was_tag)
 
+      now_tag = 1
       write_new_files_to(git_dir)
       delete_deleted_files_from(git_dir)
       overwrite_changed_files_in(git_dir)
-      git.commit(git_dir, now_tag=1)
+      git.commit(git_dir, now_tag)
 
       diff_lines = git.diff(git_dir, was_tag, now_tag)
       git_diff(diff_lines, now_files)
@@ -34,10 +33,10 @@ class Differ
 
   private
 
-  attr_reader :delta
+  attr_reader :delta, :now_files
 
   def make_empty_git_repo_in(git_dir)
-    user_name = 'nobody'
+    user_name = 'differ'
     user_email = user_name + '@cyber-dojo.org'
     git.setup(git_dir, user_name, user_email)
   end
