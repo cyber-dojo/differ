fa7afa616278c6cb6bc9a1bedbe6d54be962879d
Jon Jagger <jon@jaggersoft.com>
2016-09-10 07:47:21 +0100

    refactoring

--- server/test/test_external_helpers.rb
+++ server/test/test_external_helpers.rb
@@ -4,20 +4,16 @@ module TestExternalHelpers # mix-in
   module_function
 
   def setup
-    raise "setup already called" unless @setup_called.nil?
+    raise 'setup() already called' unless @setup_called.nil?
     @setup_called = true
     @config = {}
     env_map.keys.each { |key| @config[key] = ENV[key] }
   end
 
   def teardown
-    fail_if_setup_not_called('teardown')
+    fail 'teardown() NOT executed because setup() not yet called' if @setup_called.nil?
     env_map.keys.each { |key| ENV[key] = @config[key] }
     @setup_called = nil
-  end
-
-  def fail_if_setup_not_called(cmd)
-    fail "#{cmd} NOT executed because setup() not yet called" if @setup_called.nil?
   end
 
 end
