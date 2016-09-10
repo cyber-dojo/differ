bfefcbaa9394c34aefa63fc9acc56fd41017af89
Jon Jagger <jon@jaggersoft.com>
2016-09-10 08:23:17 +0100

    added tests for lib/external-stdout-logger

---
+++ server/test/lib/external_stdout_logger_test.rb
@@ -0,0 +1,31 @@
+#!/bin/bash ../test_wrapper.sh
+
+require_relative './lib_test_base'
+
+class ExternalStdoutLoggerTest < LibTestBase
+
+  test '1B6962',
+  '<< writes to stdout with added trailing newline' do
+    log = ExternalStdoutLogger.new(nil)
+    written = with_captured_stdout { log << "Hello world" }
+    assert_equal quoted('Hello world')+"\n", written
+  end
+
+  private
+
+  def with_captured_stdout
+    begin
+      old_stdout = $stdout
+      $stdout = StringIO.new('','w')
+      yield
+      $stdout.string
+    ensure
+      $stdout = old_stdout
+    end
+  end
+
+  def quoted(s)
+    '"' + s + '"'
+  end
+
+end
