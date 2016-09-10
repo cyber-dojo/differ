6ba864f067d54c526f1846bb93109af4dd969c89
Jon Jagger <jon@jaggersoft.com>
2016-09-10 06:03:05 +0100

    test refactoring

--- server/test/lib/external_parent_chainer_test.rb
+++ server/test/lib/external_parent_chainer_test.rb
@@ -1,30 +1,61 @@
 #!/bin/bash ../test_wrapper.sh
 
 require_relative './lib_test_base'
+require_relative './spy_logger'
 
-class LogDemo
-  def initialize(root)
-    @root = root
-    @messages = []
+class ExternalParentChainerTest < LibTestBase
+
+  def setup
+    super
+    @edna = GrandMother.new
+    @margaret = Mother.new(@edna)
+    @ellie = Daughter.new(@margaret)
   end
-  def m(*args)
-    @messages << args
-    args
+
+  attr_reader :edna, :margaret, :ellie
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  test '397B16',
+  'root object has no parent, does not include chainer, has accessible externals (eg log)' do
+    edna.log << 'Tay'
+    assert_equal ['Tay'], edna.log.spied
   end
-  def messages
-    @messages
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  test '67A467',
+  'objects are chained together using parent and paths use parent property' do
+    assert_equal 'Daughter', ellie.class.name
+    assert_equal 'Mother', ellie.parent.class.name
+    assert_equal 'GrandMother', ellie.parent.parent.class.name
   end
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  test '4271A8',
+  'method_missing finds first object without a parent and delegates to it' do
+    ellie.method(42)
+    ellie.method('hello')
+    assert_equal [42,'hello'], edna.log.spied
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - -
+
+  test '5BBF52',
+  'parent chain is for dot *access* only - passing args raises RuntimeError' do
+    assert_equal [], ellie.log.spied
+    raised = assert_raises(RuntimeError) { ellie.log(42) }
+    assert_equal "not-expecting-arguments [42]", raised.message
+  end
+
 end
 
 # - - - - - - - - - - - - - - - -
 
 class GrandMother
 
-  def path
-    'grand_mother/'
-  end
-
-  def log; @log ||= LogDemo.new(self); end
+  def log; @log ||= SpyLogger.new(self); end
 
 end
 
@@ -37,10 +68,6 @@ class Mother
   end
 
   attr_reader :parent
-
-  def path
-    parent.path + 'mother/'
-  end
 
   private
 
@@ -58,80 +85,12 @@ class Daughter
 
   attr_reader :parent
 
-  def path
-    parent.path + 'daughter/'
+  def method(arg)
+    log << arg
   end
+
+  private
 
   include ExternalParentChainer
 
 end
-
-# - - - - - - - - - - - - - - - -
-
-class ExternalParentChainerTest < LibTestBase
-
-  def setup
-    super
-    @edna = GrandMother.new
-    @margaret = Mother.new(@edna)
-    @ellie = Daughter.new(@margaret)
-  end
-
-  attr_reader :edna, :margaret, :ellie
-
-  # - - - - - - - - - - - - - - - - - - - - -
-
-  test '67A467',
-  'objects are chained together using parent and paths use parent property' do
-    assert_equal 'Daughter', ellie.class.name
-    assert_equal 'Mother', ellie.parent.class.name
-    assert_equal 'GrandMother', ellie.parent.parent.class.name
-    assert_equal 'grand_mother/', edna.path
-    assert_equal 'grand_mother/mother/', margaret.path
-    assert_equal 'grand_mother/mother/daughter/', ellie.path
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - -
-
-  test '4271A8',
-  'method_missing finds first object without a parent and delegates to it' do
-    assert_equal [1,2,3], ellie.log.m(1,2,3)
-    assert_equal ['s'], ellie.log.m('s')
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - -
-
-  test 'EA58E0',
-  'log retains its state across calls' do
-    ellie.log.m(1,2,3)
-    ellie.log.m('s')
-    margaret.log.m(6,7,8)
-    assert_equal [[1,2,3],['s'],[6,7,8]], edna.log.messages
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - -
-
-  test '5BBF52',
-  'parent chain is for object *access* only - passing args raises RuntimeError' do
-    ellie.log
-    raised = assert_raises(RuntimeError) { ellie.log(42) }
-    assert_equal "not-expecting-arguments [42]", raised.message
-    raised = assert_raises(RuntimeError) { ellie.log(4, 2) }
-    assert_equal "not-expecting-arguments [4, 2]", raised.message
-  end
-
-  # - - - - - - - - - - - - - - - - - - - - -
-
-  test '397B16',
-  'root object has no parent, does not include chainer, but still has accessible externals (log)' do
-    ellie.log.m(1,2)
-    ellie.log.m('Tay')
-    assert_equal [[1,2],['Tay']], edna.log.messages
-  end
-
-end
-
-
-
-
-
