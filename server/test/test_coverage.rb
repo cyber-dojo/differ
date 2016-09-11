aa955ea6585ca3e3a322f5dc5194ab2501aabaf4
Jon Jagger <jon@jaggersoft.com>
2016-09-11 11:12:02 +0100

    exclude test_hex_helpers.rb from coverage so code and test coverage is 100%

--- server/test/test_coverage.rb
+++ server/test/test_coverage.rb
@@ -3,7 +3,13 @@ require 'simplecov'
 SimpleCov.start do
   filters.clear
   add_group 'lib', '/usr/app/lib'
-  add_group 'test', '/usr/app/test'
+  add_group('test/lib') { |src|
+    # exclude test_hex_id_helpers.rb because it contains
+    # ObjectSpace.define_finalizer(self, proc { ... }
+    # to detect any unfound hex-id args
+    src.filename.start_with?('/usr/app/test/') &&
+    src.filename != '/usr/app/test/test_hex_id_helpers.rb'
+  }
 end
 
 cov_root = File.expand_path('..', File.dirname(__FILE__))
