a71f86ddf09b0090731ce1c2ac5ca03821c4e4f0
Jon Jagger <jon@jaggersoft.com>
2016-09-11 10:48:19 +0100

    shell stays in its cwd even if script cd's

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 docker images --quiet --filter=dangling=true | xargs docker rmi
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-CWD=${PWD##*/}
 cd ${MY_DIR}
 ./build.sh
+
 docker run --cidfile=${CIDFILE} cyberdojo/differ sh -c "cd test/lib && ./run.sh ${1}"
 CID=`docker ps --latest --quiet`
 docker cp ${CID}:/usr/app/coverage ${MY_DIR}
