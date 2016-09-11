e9f2d2dddf91a7c1a48c2b256a9266916edfe3fa
Jon Jagger <jon@jaggersoft.com>
2016-09-11 07:43:33 +0100

    added tests for lib/differ

--- server/test/lib/differ_test.rb
+++ server/test/lib/differ_test.rb
@@ -13,17 +13,27 @@ class DifferTest < LibTestBase
   # - - - - - - - - - - - - - - - - - - - -
 
   test '9552DC',
-  'empty was_files and/or now_files is benign no-op' do
+  'empty was_files and now_files is benign no-op' do
     diffs = Differ.new({},{}).diff
     assert_equal({}, diffs)
   end
 
   # - - - - - - - - - - - - - - - - - - - -
 
+  test '1870E3',
+  'deleted empty file shows empty array' do
+    was_files = { 'hiker.h' => '' }
+    now_files = {}
+    diffs = Differ.new(was_files, now_files).diff
+    assert_equal [], diffs['hiker.h']
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
   test '2142E2',
-  'file in was_files but not now_files is a deleted file' do
+  'deleted non-empty file shows as all lines deleted' do
     was_files = {
-      'hiker.h' => "a\nb\n"
+      'hiker.h' => "a\nb\nc\nd\n"
     }
     now_files = {
       'diamond.h' => ''
@@ -32,48 +42,86 @@ class DifferTest < LibTestBase
     assert_equal [
       { :line=>'a', :type=>:deleted, :number=>1 },
       { :line=>'b', :type=>:deleted, :number=>2 },
+      { :line=>'c', :type=>:deleted, :number=>3 },
+      { :line=>'d', :type=>:deleted, :number=>4 },
     ], diffs['hiker.h']
   end
 
   # - - - - - - - - - - - - - - - - - - - -
 
+  test '01CECA',
+  'added empty file shows as one empty line' do
+    was_files = {
+      'hiker.h' => "a\nb\nc\nd"
+    }
+    now_files = {
+      'diamond.h' => ''
+    }
+    diffs = Differ.new(was_files, now_files).diff
+    assert_equal [
+      { :line=>'', :type=>:same, :number=>1 },
+    ], diffs['diamond.h']
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
   test 'C888D6',
-  'file not in was_files but in now_files is a new file' do
+  'added non-empty file shows as all lines added' do
     was_files = {
       'hiker.h' => ''
     }
     now_files = {
-      'diamond.h' => "a\nb\n"
+      'diamond.h' => "a\nb\nc\nd"
     }
     diffs = Differ.new(was_files, now_files).diff
     assert_equal [
       { :type=>:section, :index=>0 },
       { :line=>'a', :type=>:added, :number=>1 },
       { :line=>'b', :type=>:added, :number=>2 },
+      { :line=>'c', :type=>:added, :number=>3 },
+      { :line=>'d', :type=>:added, :number=>4 },
+    ], diffs['diamond.h']
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  test 'B3ABA1',
+  'unchanged empty-file shows as one empty line' do
+    was_files = {
+      'diamond.h' => ''
+    }
+    now_files = {
+      'diamond.h' => ''
+    }
+    diffs = Differ.new(was_files, now_files).diff
+    assert_equal [
+      { :line=>'', :type=>:same, :number=>1 },
     ], diffs['diamond.h']
   end
 
   # - - - - - - - - - - - - - - - - - - - -
 
   test 'F1E13E',
-  'file unchanged in was_files/now_files is same' do
+  'unchanged non-empty file shows as all lines same' do
     was_files = {
-      'diamond.h' => "a\nb\n"
+      'diamond.h' => "a\nb\nc\nd"
     }
     now_files = {
-      'diamond.h' => "a\nb\n"
+      'diamond.h' => "a\nb\nc\nd"
     }
     diffs = Differ.new(was_files, now_files).diff
     assert_equal [
       { :line=>'a', :type=>:same, :number=>1 },
       { :line=>'b', :type=>:same, :number=>2 },
+      { :line=>'c', :type=>:same, :number=>3 },
+      { :line=>'d', :type=>:same, :number=>4 },
     ], diffs['diamond.h']
   end
 
   # - - - - - - - - - - - - - - - - - - - -
 
   test '6A9EDF',
-  'file changed between was_files/now_files sees deleted and added lines' do
+  'changed non-empty file shows as deleted and added lines' do
     was_files = {
       'diamond.h' => 'a'
     }
@@ -82,9 +130,61 @@ class DifferTest < LibTestBase
     }
     diffs = Differ.new(was_files, now_files).diff
     assert_equal [
-    { :type=>:section, :index=>0 },
-    { :line=>'a', :type=>:deleted, :number=>1 },
-    { :line=>'b', :type=>:added,   :number=>1 },
+      { :type=>:section, :index=>0 },
+      { :line=>'a', :type=>:deleted, :number=>1 },
+      { :line=>'b', :type=>:added,   :number=>1 },
+    ], diffs['diamond.h']
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+
+  test '9E7B9E',
+  'changed non-empty file shows as deleted and added lines ' +
+  'with each chunk in its own indexed section' do
+    was_files = {
+      'diamond.h' =>
+        [
+          '#ifndef DIAMOND',
+          '#define DIAMOND',
+          '',
+          '#include <strin>', # no g
+          '',
+          'void diamond(char)', # no ;
+          '',
+          '#endif',
+        ].join("\n")
+    }
+    now_files = {
+      'diamond.h' =>
+        [
+        '#ifndef DIAMOND',
+        '#define DIAMOND',
+        '',
+        '#include <string>',
+        '',
+        'void diamond(char);',
+        '',
+        '#endif',
+        ].join("\n")
+    }
+    diffs = Differ.new(was_files, now_files).diff
+    assert_equal [
+      { :line=>'#ifndef DIAMOND',     :type=>:same,    :number=>1 },
+      { :line=>'#define DIAMOND',     :type=>:same,    :number=>2 },
+      { :line=>'',                    :type=>:same,    :number=>3 },
+
+      { :type=>:section, :index=>0 },
+      { :line=>'#include <strin>',    :type=>:deleted, :number=>4 },
+      { :line=>'#include <string>',   :type=>:added,   :number=>4 },
+
+      { :line=>'',                    :type=>:same,    :number=>5 },
+
+      { :type=>:section, :index=>1 },
+      { :line=>'void diamond(char)',  :type=>:deleted, :number=>6 },
+      { :line=>'void diamond(char);', :type=>:added,   :number=>6 },
+
+      { :line=>'',                    :type=>:same,    :number=>7 },
+      { :line=>'#endif',              :type=>:same,    :number=>8 },
     ], diffs['diamond.h']
   end
 
