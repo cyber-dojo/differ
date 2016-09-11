865eec470dc98f2b6782ad0616f58c0024e458b6
Jon Jagger <jon@jaggersoft.com>
2016-09-11 15:34:55 +0100

    cat Gemfile.lock in test script

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ docker images --quiet --filter=dangling=true | xargs docker rmi
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 cd ${MY_DIR}
 ./build.sh
-
+docker run --rm cyberdojo/differ sh -c 'cat /usr/app/Gemfile.lock'
 docker run --cidfile=${CIDFILE} cyberdojo/differ sh -c "cd test/lib && ./run.sh ${1}"
 CID=`docker ps --latest --quiet`
 docker cp ${CID}:/usr/app/coverage ${MY_DIR}
