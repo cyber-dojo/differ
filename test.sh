fdb128812b08a8c8a5ea89be876803cefe6d6e12
Jon Jagger <jon@jaggersoft.com>
2016-09-11 10:26:44 +0100

    ensure test.sh removes its docker container

--- test.sh
+++ test.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 docker images --quiet --filter=dangling=true | xargs docker rmi
 ./build.sh
+CIDFILE=`mktemp -t cyber-dojo.cid.XXXXXX`
+rm ${CIDFILE}
 docker run --cidfile=${CIDFILE} cyberdojo/differ sh -c "cd test/lib && ./run.sh ${1}"
+CID=`cat ${CIDFILE}`
+docker rm ${CID} > /dev/null
 
-#CIDFILE=`mktemp -t cyber-dojo.cid.XXXXXX`
-#rm ${CIDFILE}
-#docker run --cidfile=${CIDFILE} cyberdojo/differ sh -c "cd test/lib && ./run.sh ${1}"
-#CID=`cat ${CIDFILE}`
-#docker exec ${CID} sh -c 'cat /usr/app/coverage/....'
-#docker rm ${CID}
