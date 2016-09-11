10860f3b5841a11595efbcf5014ab3715cd6e550
Jon Jagger <jon@jaggersoft.com>
2016-09-11 10:45:10 +0100

    copy coverage out of from test container before removing it

--- server/test/test_coverage.rb
+++ server/test/test_coverage.rb
@@ -2,7 +2,8 @@ require 'simplecov'
 
 SimpleCov.start do
   filters.clear
-  add_group 'lib', 'lib'
+  add_group 'lib', '/usr/app/lib'
+  add_group 'test', '/usr/app/test'
 end
 
 cov_root = File.expand_path('..', File.dirname(__FILE__))
