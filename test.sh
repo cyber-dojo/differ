33849c19e641da1f7a1d905ee2c45daebea72b87
Jon Jagger <jon@jaggersoft.com>
2016-09-11 10:30:29 +0100

    refactor test.sh to use [docker ps -lq] to get cid

--- test.sh
+++ test.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 docker images --quiet --filter=dangling=true | xargs docker rmi
 ./build.sh
-CIDFILE=`mktemp -t cyber-dojo.cid.XXXXXX`
-rm ${CIDFILE}
 docker run --cidfile=${CIDFILE} cyberdojo/differ sh -c "cd test/lib && ./run.sh ${1}"
-CID=`cat ${CIDFILE}`
+CID=`docker ps --latest --quiet`
 docker rm ${CID} > /dev/null
-
