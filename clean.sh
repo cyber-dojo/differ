2fd4120cca1cedb0fd382f8c154c50b59a44043c
Jon Jagger <jon@jaggersoft.com>
2016-09-12 11:34:12 +0100

    remove dead container in clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker images --quiet --filter=dangling=true | xargs docker rmi
+# remove images
+docker images --quiet --filter dangling=true | xargs docker rmi --force
+# remove containers
+docker ps     --quiet --filter status=exited | xargs docker rm --force
