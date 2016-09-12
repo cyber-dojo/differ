1056b69e1753e3c245c15efa9653caf4acd9568e
Jon Jagger <jon@jaggersoft.com>
2016-09-12 09:17:51 +0100

    refactoring; shell file improvements

--- up.sh
+++ up.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -e
 
-./build.sh
-if [ $? != 0 ]; then
-  exit 1
-fi
-
+docker images --quiet --filter=dangling=true | xargs docker rmi
+MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+cd ${MY_DIR} && ./build.sh
 ip=$(docker-machine ip default)
 echo "${ip}:4568/diff"
-
-docker images --quiet --filter=dangling=true | xargs docker rmi
-
 docker-compose up &
-
-
