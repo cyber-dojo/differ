05617e6d6a7ed083ab5e7aefec0263cb753392a6
Jon Jagger <jon@jaggersoft.com>
2016-09-12 09:15:23 +0100

    refactoring; delete dead code

--- test.sh
+++ test.sh
@@ -3,10 +3,9 @@ set -e
 docker images --quiet --filter=dangling=true | xargs docker rmi
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-cd ${MY_DIR}
-./build.sh
+cd ${MY_DIR} && ./build.sh
 docker run --rm cyberdojo/differ sh -c 'cat /usr/app/Gemfile.lock'
-docker run --cidfile=${CIDFILE} cyberdojo/differ sh -c "cd test/src && ./run.sh ${1}"
+docker run cyberdojo/differ sh -c "cd test/src && ./run.sh ${1}"
 CID=`docker ps --latest --quiet`
 docker cp ${CID}:/usr/app/coverage ${MY_DIR}
 docker rm ${CID} > /dev/null
