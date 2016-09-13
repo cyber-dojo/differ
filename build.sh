30b7eeacdf8736ed8a20c378b5f1e1037d9eb24e
Jon Jagger <jon@jaggersoft.com>
2016-09-13 16:51:02 +0100

    delete dead comments

--- build.sh
+++ build.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 set -e
 
-exit_if_not_installed() {
-  # Note that .travis.yml does fake installs
-  # of docker-machine and docker-compose.
-  hash ${1} 2> /dev/null
-  if [ $? != 0 ]; then
-    echo
-    echo "${1} is not installed"
-    exit 1
-  fi
-}
-
-exit_if_not_installed 'docker'
-
-#exit_if_not_installed 'docker-machine'
-#exit_if_not_installed 'docker-compose'
+hash docker 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker is not installed"
+  exit 1
+fi
 
 MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 
