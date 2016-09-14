0ed4f9923ebb41ad37a74941ed71dd3f42418a59
Jon Jagger <jon@jaggersoft.com>
2016-09-14 08:12:36 +0100

    refactoring; remove unneeded test code

--- server/test/src/name_of_caller_test.rb
+++ server/test/src/name_of_caller_test.rb
@@ -7,28 +7,24 @@ class NameOfCallerTest < LibTestBase
   test '07ADA9',
   'name of caller is name of callers method' do
     assert_equal 'helper1', helper1
+    assert_equal 'helper2', helper2
   end
 
   test '3615C2',
   'name of caller is name of callers method' do
-    assert_equal 'helper3', helper3
   end
 
   private
 
   def helper1
-    helper2
+    helper
   end
 
   def helper2
-    name_of(caller)
+    helper
   end
 
-  def helper3
-    helper4
-  end
-
-  def helper4
+  def helper
     name_of(caller)
   end
 
