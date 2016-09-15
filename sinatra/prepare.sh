be91e1fe9af082a16119360b8d4ecbad7060362c
Jon Jagger <jon@jaggersoft.com>
2016-09-15 10:48:41 +0100

    added gem clean to Dockerfile's prepare.sh to shave off a few K from image size

--- sinatra/prepare.sh
+++ sinatra/prepare.sh
@@ -9,7 +9,7 @@ echo 'gem: --no-document' > ~/.gemrc #http://stackoverflow.com/questions/1381725
 apk --update \
         add --virtual build-dependencies \
           build-base \
-        && bundle install --without development test \
+        && bundle install && gem clean \
         && apk del build-dependencies
 
 rm -vrf /var/cache/apk/*
