912ac638e027ddcff2e8584bffc2958065985a6e
Jon Jagger <jon@jaggersoft.com>
2016-09-15 15:50:15 +0100

    refactoring; simplify server Dockerfile

--- server/prepare.sh
+++ server/prepare.sh
@@ -2,6 +2,9 @@
 
 export RACK_ENV='production'
 
+# server/test/src/run.sh needs bash
+apk --update add ruby ruby-dev ruby-io-console ruby-bundler git bash
+
 echo 'gem: --no-document' > ~/.gemrc #http://stackoverflow.com/questions/1381725
 
 apk --update \
--- server/prepare_platform.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# server/test/src/run.sh needs bash
-apk --update add ruby ruby-dev ruby-io-console ruby-bundler git bash
-rm -v ${0}
