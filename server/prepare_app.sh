5d61a2c49c128abc0c077247a2d0c26a2d7c747b
Jon Jagger <jon@jaggersoft.com>
2016-09-15 13:27:37 +0100

    split Dockerfile's prepare script into two to speed up dev cycle

--- server/prepare_app.sh
+++ server/prepare_app.sh
@@ -2,9 +2,6 @@
 
 export RACK_ENV='production'
 
-# server/test/src/run.sh needs bash
-apk --update add ruby ruby-dev ruby-io-console ruby-bundler git bash
-
 echo 'gem: --no-document' > ~/.gemrc #http://stackoverflow.com/questions/1381725
 
 apk --update \
