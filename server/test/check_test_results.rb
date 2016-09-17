e6f478a9b9195b711d1297dd0d6f41a91a767175
Jon Jagger <jon@jaggersoft.com>
2016-09-17 12:25:37 +0100

    refactoring; use tabular format for test done reporting

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -72,42 +72,48 @@ log_stats = get_test_log_stats
 test_stats = get_index_stats('testsrc', 'test/src')
 src_stats = get_index_stats('src', 'src')
 
+# - - - - - - - - - - - - - - - - - - - - - - -
+
+failure_count = log_stats[:failure_count]
+error_count   = log_stats[:error_count]
+skip_count    = log_stats[:skip_count]
+
 test_duration = log_stats[:time].to_f
 assertions_per_sec = log_stats[:assertions_per_sec].to_i
+
+src_coverage = src_stats[:coverage]
+test_coverage = test_stats[:coverage]
+
 hits_per_line_src = src_stats[:hits_per_line].to_f
 hits_per_line_test = test_stats[:hits_per_line].to_f
 line_ratio = (test_stats[:line_count].to_f / src_stats[:line_count].to_f)
 
+# - - - - - - - - - - - - - - - - - - - - - - -
+
 done =
   [
-     [ 'failures == 0', log_stats[:failure_count] <= 0 ],
-     [ 'errors == 0', log_stats[:error_count] == 0 ],
-     [ 'skips == 0', log_stats[:skip_count] == 0],
-     [ "test duration < 1 [#{test_duration}]", test_duration < 1 ],
-     [ "assertions per sec > 200 [#{assertions_per_sec}]", assertions_per_sec > 200 ],
-     [ 'coverage(src) == 100%', src_stats[:coverage] == '100.00'],
-     [ 'coverage(test) == 100%', test_stats[:coverage] == '100.00'],
-     [ "hits_per_line(src) < 60 [#{hits_per_line_src}]", hits_per_line_src < 60 ],
-     [ "hits_per_line(test) < 2 [#{hits_per_line_test}]", hits_per_line_test < 2 ],
-     [ "lines(test)/lines(src) > 2 [#{f2 line_ratio}]", line_ratio > 2 ],
+    [ 'failures',               failure_count,      '== 0',    failure_count == 0       ],
+    [ 'errors',                 error_count,        '== 0',    error_count == 0         ],
+    [ 'skips',                  skip_count,         '== 0',    skip_count == 0          ],
+    [ 'test duration',          test_duration,      '< 1',     test_duration < 1        ],
+    [ 'assertions per sec',     assertions_per_sec, '> 200',   assertions_per_sec > 200 ],
+    [ 'coverage(src)',          src_coverage,       '== 100%', src_coverage == '100.00' ],
+    [ 'coverage(test)',         test_coverage,      '== 100%', test_coverage == '100.00'],
+    [ 'hits_per_line(src)',     hits_per_line_src,  '< 60',    hits_per_line_src < 60   ],
+    [ 'hits_per_line(test)',    hits_per_line_test, '< 2',     hits_per_line_test < 2   ],
+    [ 'lines(test)/lines(src)', f2(line_ratio),     '> 2',     line_ratio > 2           ],
   ]
-
-yes,no = done.partition { |criteria| criteria[1] }
 
 # - - - - - - - - - - - - - - - - - - - - - - -
 
-unless yes.empty?
-  print "\n"
-  puts "DONE"
-  yes.each { |criteria| puts '  ' + criteria[0] }
+print "\n"
+done.each do |name,value,predicate,result|
+  puts "%s | %s | %s | %s" % [
+    name.rjust(25),
+    value.to_s.rjust(7),
+    predicate.ljust(8),
+    result.to_s
+  ]
 end
 
-unless no.empty?
-  print "\n"
-  puts "NOT-DONE"
-  no.each { |criteria| puts '  ' + criteria[0] }
-  exit 1
-else
-  exit 0
-end
-
+exit done.all?{ |entry| entry[3] }
