8e4b976dc28cb4ee1ff0462bfefd9bf6f44ecf9b
Jon Jagger <jon@jaggersoft.com>
2016-09-24 06:48:00 +0100

    refactoring; lose unneeded @ness

--- server/src/micro_service.rb
+++ server/src/micro_service.rb
@@ -14,9 +14,9 @@ class MicroService < Sinatra::Base
   get '/' do
     content_type :json
     request.body.rewind
-    @args = JSON.parse(request.body.read)
-    was_files = @args['was_files']
-    now_files = @args['now_files']
+    args = JSON.parse(request.body.read)
+    was_files = args['was_files']
+    now_files = args['now_files']
     diff = GitDiffer.new(self).diff(was_files, now_files)
     git_diff_view(diff, now_files).to_json
   end
