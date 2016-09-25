f75c93fdfe5b221153b43e5d6a66025f0c21af28
Jon Jagger <jon@jaggersoft.com>
2016-09-25 08:31:51 +0100

    refactoring; exception-safety too vague a term, moved test

--- server/test/src/git_differ_test.rb
+++ server/test/src/git_differ_test.rb
@@ -3,6 +3,7 @@
 
 require_relative './lib_test_base'
 require_relative './null_logger'
+require_relative './raising_disk_writer'
 
 class GitDifferTest < LibTestBase
 
@@ -14,7 +15,23 @@ class GitDifferTest < LibTestBase
 
   def setup
     super
-    ENV['DIFFER_CLASS_LOG'] = 'NullLogger'
+    ENV[env_name('log')]  = 'NullLogger'
+  end
+
+  # - - - - - - - - - - - - - - - - - - - -
+  # exception safety
+  # - - - - - - - - - - - - - - - - - - - -
+
+  test 'B9F',
+  'tmp dir is deleted if exception is raised' do
+    ENV[env_name('disk')] = 'RaisingDiskWriter'
+    was_files = { 'diamond.h' => 'a' } # ensure something to write
+    now_files = {}
+    differ = GitDiffer.new(self)
+    raised = assert_raises(RuntimeError) { differ.diff(was_files, now_files) }
+    assert_equal 'raising', raised.message
+    dir = File.dirname(differ.disk.pathed_filename)
+    refute Dir.exists?(dir)
   end
 
   # - - - - - - - - - - - - - - - - - - - -
