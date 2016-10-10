1305b5f7b18272bd5bea312a00bdbf562ffbb17e
Jon Jagger <jon@jaggersoft.com>
2016-10-10 22:47:05 +0100

    fix demo.sh

--- demo.sh
+++ demo.sh
@@ -2,9 +2,15 @@
 set -e
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
-cd ${my_dir} && ./build.sh
-ip=$(docker-machine ip default)
-echo "${ip}:4568/diff"
+app_dir=/app
+client_port=4568
+server_port=4567
 
+${my_dir}/build.sh ${app_dir} ${client_port} ${server_port}
+ip=$(docker-machine ip default)
+echo "${ip}:${client_port}"
+
+export CLIENT_PORT=${client_port}
+export SERVER_PORT=${server_port}
 docker-compose down
 docker-compose up &
