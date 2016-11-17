ee5c7182b840af25949fd05e2ece6c0dabf78f0c
Jon Jagger <jon@jaggersoft.com>
2016-11-17 13:23:51 +0000

    test docker and docker-compose are installed at top of build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-set -e
+
+hash docker 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker is not installed"
+  exit 1
+fi
+
+hash docker-compose 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker-compose is not installed"
+  exit 1
+fi
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
