c1cc38887865077cfbe3007ef00b6ef5232a61e3
Jon Jagger <jon@jaggersoft.com>
2016-11-27 07:46:27 +0000

    use -- long options in scripts

--- build.sh
+++ build.sh
@@ -16,5 +16,5 @@ fi
 
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
-docker-compose -f ${my_dir}/client/docker-compose.yml build
-docker-compose -f ${my_dir}/server/docker-compose.yml build
+docker-compose --file ${my_dir}/client/docker-compose.yml build
+docker-compose --file ${my_dir}/server/docker-compose.yml build
