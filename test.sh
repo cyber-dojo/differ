f1e9ca77a1852c778b3fc4b003aff3334988e698
Jon Jagger <jon@jaggersoft.com>
2017-02-21 14:47:12 +0000

    refactoring; remove script duplication

--- test.sh
+++ test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-server_cid=`docker ps --all --quiet --filter "name=differ_server"`
+my_dir="${PWD}"
+my_name="${PWD##*/}"
+
+server_cid=`docker ps --all --quiet --filter "name=${my_name}_server"`
 server_status=0
 
-client_cid=`docker ps --all --quiet --filter "name=differ_client"`
+client_cid=`docker ps --all --quiet --filter "name=${my_name}_client"`
 client_status=0
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
 run_server_tests()
 {
@@ -16,7 +17,7 @@ run_server_tests()
   docker exec ${server_cid} sh -c "cd test && ./run.sh ${*}"
   server_status=$?
   docker cp ${server_cid}:/tmp/coverage ${my_dir}/server
-  echo "Coverage report copied to ${my_dir}/server/coverage"
+  echo "Coverage report copied to ${my_name}/server/coverage"
   cat ${my_dir}/server/coverage/done.txt
 }
 
@@ -28,7 +29,7 @@ run_client_tests()
   docker exec ${client_cid} sh -c "cd test && ./run.sh ${*}"
   client_status=$?
   docker cp ${client_cid}:/tmp/coverage ${my_dir}/client
-  echo "Coverage report copied to ${my_dir}/client/coverage"
+  echo "Coverage report copied to ${my_name}/client/coverage"
   cat ${my_dir}/client/coverage/done.txt
 }
 
@@ -46,11 +47,11 @@ else
   echo
   echo "server: cid = ${server_cid}, status = ${server_status}"
   if [ "${server_cid}" != "0" ]; then
-    docker logs differ_server
+    docker logs ${my_name}_server
   fi
   echo "client: cid = ${client_cid}, status = ${client_status}"
   if [ "${client_cid}" != "0" ]; then
-    docker logs differ_client
+    docker logs ${my_name}_client
   fi
   echo
   exit 1
