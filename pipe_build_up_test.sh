8577f3e91e75af9caa13024876fc924dd0d5aca0
Jon Jagger <jon@jaggersoft.com>
2017-10-16 10:22:00 +0100

    bring containers down before rebuilding images to clear out stale containers

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,6 +3,7 @@ set -e
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
+${SH_DIR}/docker_containers_down.sh
 ${SH_DIR}/build_docker_images.sh
 ${SH_DIR}/docker_containers_up.sh
 ${SH_DIR}/run_tests_in_containers.sh ${*}
