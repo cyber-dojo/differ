d9dbe20ca43f71746cede89e930601377faf71b1
Jon Jagger <jon@jaggersoft.com>
2017-11-10 07:17:42 +0000

    bump test metric

--- server/test/check_test_results.rb
+++ server/test/check_test_results.rb
@@ -97,7 +97,7 @@ table =
     [ 'failures',               failure_count,      '==',   0 ],
     [ 'errors',                 error_count,        '==',   0 ],
     [ 'skips',                  skip_count,         '==',   0 ],
-    [ 'assertions/s',           assertions_per_sec, '>=', 100 ],
+    [ 'assertions/s',           assertions_per_sec, '>=',  80 ],
     [ 'duration(test)[s]',      test_duration,      '<=',   3 ],
     [ 'coverage(src)[%]',       src_coverage,       '==', 100 ],
     [ 'coverage(test)[%]',      test_coverage,      '==', 100 ],
