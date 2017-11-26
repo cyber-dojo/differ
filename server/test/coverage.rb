8a7fa2f519f8d8e0a72d6219cc2d392a6749a041
Jon Jagger <jon@jaggersoft.com>
2017-11-26 15:49:29 +0000

    refactoring; test improvements

--- server/test/coverage.rb
+++ server/test/coverage.rb
@@ -5,9 +5,13 @@ cov_root = File.expand_path('..', File.dirname(__FILE__))
 SimpleCov.start do
   #add_group('debug') { |src| print src.filename+"\n"; false }
   add_filter 'src/micro_service.rb' # done from client container
-  add_group('src')      { |src| src.filename.start_with? "#{cov_root}/src" }
-  add_group('test/src') { |src| src.filename.start_with? "#{cov_root}/test/src" }
+  add_group('src')      { |src|
+    src.filename.start_with? "#{cov_root}/src"
+  }
+  add_group('test/src') { |src|
+    src.filename.start_with? "#{cov_root}/test/src"
+  }
 end
 
 SimpleCov.root cov_root
-SimpleCov.coverage_dir '/tmp/coverage'
+SimpleCov.coverage_dir ENV['CYBER_DOJO_COVERAGE_ROOT']
