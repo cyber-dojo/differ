198710765741666add4359f00ac0288653aea17e
Jon Jagger <jon@jaggersoft.com>
2017-11-26 15:18:27 +0000

    refactoring; set env=production in rackup call and not in Dockerfile

--- client/up.sh
+++ client/up.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-bundle exec rackup --host 0.0.0.0 -p ${PORT}
+# Note that the --host is needed for IPv4 and IPv6 addresses
+
+bundle exec rackup \
+  --warn \
+  --host 0.0.0.0 \
+  --port ${PORT} \
+  --server thin \
+  --env production \
+    config.ru
