58eb44f8d7424eec1badd1bb9d4e9477d0aef7c8
Jon Jagger <jon@jaggersoft.com>
2017-11-26 08:50:15 +0000

    refactoring; now explicitly waiting till containers are up

--- sh/docker_containers_up.sh
+++ sh/docker_containers_up.sh
@@ -3,4 +3,22 @@ set -e
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
+wait_till_up()
+{
+  local n=10
+  while [ $(( n -= 1 )) -ge 0 ]
+  do
+    if docker ps --filter status=running --format '{{.Names}}' | grep -q ^${1}$ ; then
+      return
+    else
+      sleep 0.5
+    fi
+  done
+  echo "${1} not up after 5 seconds"
+  docker logs ${1}
+  exit 1
+}
+
 docker-compose --file ${ROOT_DIR}/docker-compose.yml up -d
+wait_till_up 'differ_server'
+wait_till_up 'differ_client'
