dceb9954932f5c3a2b5ad02ac16989a328af482e
Jon Jagger <jon@jaggersoft.com>
2018-04-29 16:33:03 +0100

    put file containing git sha into the docker image

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,6 +3,8 @@ set -e
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
+export SHA=$(cd "${SH_DIR}" && git rev-parse HEAD)
+
 "${SH_DIR}/docker_containers_down.sh"
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
