2b815010745021a6956c92ddb4949ce531248e28
Jon Jagger <jon@jaggersoft.com>
2018-08-28 07:49:10 +0200

    ensure sha.txt get inside docker image

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,6 +2,8 @@
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
+export SHA=$(cd "${SH_DIR}" && git rev-parse HEAD)
+
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
   build
