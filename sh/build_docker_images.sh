ce2f4072ed9e5bf65a12b35304590858d39ce39f
Jon Jagger <jon@jaggersoft.com>
2018-08-28 07:57:00 +0200

    use correct env-var in script; delete dead code

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -2,7 +2,7 @@
 
 readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 
-export SHA=$(cd "${SH_DIR}" && git rev-parse HEAD)
+export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
