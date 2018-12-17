46f22e7efc5a162d7c52fbfd8d281033591ddbad
Jon Jagger <jon@jaggersoft.com>
2018-12-17 15:02:39 +0000

    refactoring

--- client/test/util/coverage.rb
+++ client/test/util/coverage.rb
@@ -8,8 +8,8 @@ SimpleCov.start do
   add_group('src') { |src|
     src.filename.start_with? "#{cov_root}/src"
   }
-  add_group('test') { |src|
-    src.filename.start_with? "#{cov_root}/test"
+  add_group('test') { |test|
+    test.filename.start_with? "#{cov_root}/test"
   }
 end
 
--- client/test/util/run.sh
+++ client/test/util/run.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-if [ ! -f /.dockerenv ]; then
-  echo 'FAILED: run.sh is being executed outside of docker-container.'
-  echo 'Use pipe_build_up_test.sh'
-  exit 1
-fi
-
-readonly ARGS=(${*})
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly TEST_FILES=(${MY_DIR}/../*_test.rb)
 readonly TEST_LOG=${COVERAGE_ROOT}/test.log
+readonly TEST_ARGS=(${*})
 
 mkdir -p ${COVERAGE_ROOT}
-cd ${MY_DIR}/..
-readonly FILES=(*_test.rb)
 
-ruby -e "([ './util/coverage.rb' ] + %w(${FILES[*]})).each{ |file| require './'+file }" \
-  -- ${ARGS[@]} | tee ${TEST_LOG}
+ruby \
+  -e "([ '${MY_DIR}/coverage.rb' ] + \
+         %w(${TEST_FILES[*]})        \
+      ).each{ |file| require file }" \
+  -- ${TEST_ARGS[@]}                 \
+  | tee ${TEST_LOG}
 
 ruby ${MY_DIR}/check_test_results.rb \
   ${TEST_LOG} \
