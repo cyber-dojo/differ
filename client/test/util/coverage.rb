ddc64730705ecbcbb6bc34306d79170e11b5ba41
Jon Jagger <jon@jaggersoft.com>
2018-12-18 10:05:33 +0000

    refactoring; use explicit () in rb calls

--- client/test/util/coverage.rb
+++ client/test/util/coverage.rb
@@ -3,7 +3,7 @@ require 'simplecov'
 cov_root = File.expand_path('../..', File.dirname(__FILE__))
 
 SimpleCov.start do
-  #add_group('debug') { |src| print src.filename+"\n"; false }
+  #add_group('debug') { |src| print(src.filename+"\n"); false }
   add_filter 'src/demo.rb'
   add_group('src') { |src|
     src.filename.start_with? "#{cov_root}/src"
