21ba7408d1ff116bcc0c1f40d88467813c943d3a
Jon Jagger <jon@jaggersoft.com>
2018-12-24 21:41:48 +0000

    tidy client-side Dockefile and up.sh files

--- client/up.sh
+++ client/up.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Note that the --host is needed for IPv4 and IPv6 addresses
-
-bundle exec rackup \
-  --warn \
-  --host 0.0.0.0 \
-  --port 4568 \
-  --server thin \
+rackup             \
   --env production \
+  --host 0.0.0.0   \
+  --port 4568      \
+  --server thin    \
+  --warn           \
     config.ru
