5b6a47c144ce2cfe78ae1c5e67b508278423b75e
Jon Jagger <jon@jaggersoft.com>
2018-12-24 21:25:03 +0000

    refactoring; up.sh does not need [bundle exec]

--- up.sh
+++ up.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# [1] avoids warnings such as
-#   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
-# important for tests asserting container log contents at startup.
-
-HOME=/tmp `#[1]`     \
-  bundle exec rackup  \
-    --env production  \
-    --host 0.0.0.0    \
-    --port 4567       \
-    --server thin     \
-    --warn            \
-      config.ru
+rackup  \
+  --env production  \
+  --host 0.0.0.0    \
+  --port 4567       \
+  --server thin     \
+  --warn            \
+    config.ru
