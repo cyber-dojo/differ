bd2de5e6adc28c45f78500a352f89894e248f698
Jon Jagger <jon@jaggersoft.com>
2018-12-24 16:45:42 +0000

    avoid bundle warnings in up.sh

--- up.sh
+++ up.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Note that the --host is needed for IPv4 and IPv6 addresses
+# HOME=/tmp is needed to avoid warnings such as
+#   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
+# This is important for tests that assert on container log contents at startup.
 
-bundle exec rackup \
-  --warn \
-  --host 0.0.0.0 \
-  --port 4567 \
-  --server thin \
+# --host is needed for IPv4 and IPv6 addresses
+
+HOME=/tmp bundle exec rackup \
+  --warn           \
+  --host 0.0.0.0   \
+  --port 4567      \
+  --server thin    \
   --env production \
     config.ru
-
