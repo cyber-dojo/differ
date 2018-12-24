c7ba67b60f4c7cb8448a8a9440bb203fabaddb94
Jon Jagger <jon@jaggersoft.com>
2018-12-24 17:23:48 +0000

    refactoring

--- up.sh
+++ up.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# HOME=/tmp is needed to avoid warnings such as
+# [1] avoids warnings such as
 #   Bundler will use `/tmp/bundler/home/unknown' as your home directory temporarily
-# This is important for tests that assert on container log contents at startup.
+# important for tests asserting container log contents at startup.
 
-# --host is needed for IPv4 and IPv6 addresses
-
-HOME=/tmp bundle exec rackup \
-  --warn           \
-  --host 0.0.0.0   \
-  --port 4567      \
-  --server thin    \
-  --env production \
-    config.ru
+HOME=/tmp `#[1]`     \
+  bundle exec rackup \
+    --warn           \
+    --host 0.0.0.0   \
+    --port 4567      \
+    --server thin    \
+    --env production \
+      config.ru
