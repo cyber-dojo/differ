abb791e99c7fb0ce4aee1bd8167b7ce64012b58c
Jon Jagger <jon@jaggersoft.com>
2019-03-23 13:39:15 +0000

    in sh/run_demo.sh check for docker-toolbox use

--- sh/run_demo.sh
+++ sh/run_demo.sh
@@ -5,5 +5,10 @@ readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
 
-echo 'port=4568'
+if [ ! -z "${DOCKER_MACHINE_NAME}" ]; then
+  declare ip=$(docker-machine ip "${DOCKER_MACHINE_NAME}")
+else
+  declare ip=localhost
+fi
 
+echo "demo -> ${ip}:4568"
