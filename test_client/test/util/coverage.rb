07be76cde103395c96d0e2b6ebde142ad666f239
Jon Jagger <jon@jaggersoft.com>
2019-06-25 16:23:49 +0100

    refactoring; script tidying; delete dead file

--- test_client/test/util/coverage.rb
+++ test_client/test/util/coverage.rb
@@ -3,8 +3,8 @@ require 'simplecov'
 cov_root = File.expand_path('../..', __dir__)
 SimpleCov.start do
   add_filter('src/demo.rb')
-  add_group( 'src') { | src|  src.filename.start_with? "#{cov_root}/src"  }
-  add_group('test') { |test| test.filename.start_with? "#{cov_root}/test" }
+  add_group( 'src') { | src|  src.filename.start_with?("#{cov_root}/src" ) }
+  add_group('test') { |test| test.filename.start_with?("#{cov_root}/test") }
 end
 SimpleCov.root(cov_root)
 SimpleCov.coverage_dir(ENV['COVERAGE_ROOT'])
