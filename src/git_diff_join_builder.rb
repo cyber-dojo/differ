2362c53dbfe462199179e0f064e5cb03b1a602bc
Jon Jagger <jon@jaggersoft.com>
2019-07-13 06:34:19 +0100

    refactoring; in set_nil() use a range to avoid +index-1 offset in each iteration

--- src/git_diff_join_builder.rb
+++ src/git_diff_join_builder.rb
@@ -1,21 +1,25 @@
-# Combines diff and lines to build a data structure that
+# Combines diff and old_lines to build a data structure that
 # containes a complete diff-view of a joined back up file;
 #    the lines that were deleted
 #    the lines that were added
 #    the lines that were unchanged
 #
 # diff: created from GitDiffParser. The diff between two tags (run-tests) of a file.
-# lines: an array containing the current content of the diffed file.
+# old_lines: an array containing the old content of the diffed file.
+#
+# Notes:
+# o) a diff's chunk range specifies line-numbers which are 1-based
+# o) the array of lines is 0-based
 
-module GitDiffJoinBuilder # mix-in
+module GitDiffJoinBuilder
 
   module_function
 
   def git_diff_join_builder(diff, old_lines)
 
     diff[:chunks].each.with_index do |chunk,index|
-      old_start_line = chunk[:old_start_line] # 1-based
-      new_start_line = chunk[:new_start_line] # 1-based
+      old_start_line = chunk[:old_start_line]
+      new_start_line = chunk[:new_start_line]
       set_nil(old_lines, old_start_line, chunk[:deleted].size)
       section = [ { :type => :section, index:index } ]
       section += lines(old_start_line, chunk, :deleted)
@@ -37,7 +41,9 @@ module GitDiffJoinBuilder # mix-in
   private
 
   def set_nil(old_lines, start_line, size)
-    size.times { |index| old_lines[start_line + index - 1] = nil }
+    lo = start_line - 1
+    hi = lo + size
+    (lo...hi).each { |number| old_lines[number] = nil }
   end
 
   def lines(start_line, chunk, symbol)
