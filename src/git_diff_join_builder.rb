5c7cb290967102c724db32d136dcdc5ce2b3c1e0
Jon Jagger <jon@jaggersoft.com>
2019-07-14 08:39:39 +0100

    refactoring; extract methods

--- src/git_diff_join_builder.rb
+++ src/git_diff_join_builder.rb
@@ -19,38 +19,48 @@ module GitDiffJoinBuilder
     result = old_lines.collect.with_index(1) do |line,number|
       [ { :type => :same, line:line, number:number } ]
     end
-    result.unshift([])
+    result.unshift([]) # make result 1-based to match range line-numbers
 
     diff[:hunks].each.with_index do |hunk,index|
-      old_start_line = hunk[:old_start_line]
-      new_start_line = hunk[:new_start_line]
-      section = [ { :type => :section, index:index } ]
-      section += lines(hunk, old_start_line, :deleted)
-      section += lines(hunk, new_start_line, :added)
-      old_end_line = old_start_line + hunk[:deleted].size
-      (old_start_line...old_end_line).each do |number|
-        result[number] = []
-      end
-      result[old_start_line] += section
+      remove_deleted_lines(result, hunk)
+      result[hunk[:old_start_line]] += make_section(hunk, index)
     end
 
     result.flatten!
+    new_line_numbers(result)
+    result
+  end
+
+  private
+
+  def remove_deleted_lines(result, hunk)
+    old_start_line = hunk[:old_start_line]
+    old_end_line = old_start_line + hunk[:deleted].size
+    (old_start_line...old_end_line).each do |line_number|
+      result[line_number] = []
+    end
+  end
+
+  def make_section(hunk, index)
+    section = [ { :type => :section, index:index } ]
+    section += lines(hunk, hunk[:old_start_line], :deleted)
+    section += lines(hunk, hunk[:new_start_line], :added)
+  end
+
+  def lines(hunk, start_line, symbol)
+    hunk[symbol].collect.with_index(start_line) do |line,number|
+      { type:symbol, line:line, number:number }
+    end
+  end
+
+  def new_line_numbers(result)
+    # so they are based on new_files and not old_files
     line_number = 0
     result.each do |entry|
       if entry[:type] === :same || entry[:type] === :added
         line_number += 1
         entry[:number] = line_number
       end
-    end
-
-    result
-  end
-
-  private
-
-  def lines(hunk, start_line, symbol)
-    hunk[symbol].collect.with_index(start_line) do |line,number|
-      { type:symbol, line:line, number:number }
     end
   end
 
