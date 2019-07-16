16df1363c5b6e24f2f0fc13ce3301ba0b20df267
Jon Jagger <jon@jaggersoft.com>
2019-07-16 20:54:35 +0100

    added tests for rack-dispatcher ready/sha

--- test_server/rack_dispatcher_test.rb
+++ test_server/rack_dispatcher_test.rb
@@ -10,7 +10,7 @@ class RackDispatcherTest < DifferTestBase
 
   # - - - - - - - - - - - - - - - - -
 
-  class DifferShaStub
+  class DifferShaRaiser
     def initialize(*args)
       @klass = args[0]
       @message = args[1]
@@ -22,26 +22,50 @@ class RackDispatcherTest < DifferTestBase
 
   test 'F1A',
   'dispatch returns 500 status when implementation raises' do
-    @differ_stub = DifferShaStub.new(ArgumentError, 'wibble')
+    @differ = DifferShaRaiser.new(ArgumentError, 'wibble')
     assert_dispatch_raises('sha', {}.to_json, 500, 'wibble')
   end
 
-  # - - - - - - - - - - - - - - - - -
-
   test 'F1B',
   'dispatch returns 500 status when implementation has syntax error' do
-    @differ_stub = DifferShaStub.new(SyntaxError, 'fubar')
+    @differ = DifferShaRaiser.new(SyntaxError, 'fubar')
     assert_dispatch_raises('sha', {}.to_json, 500, 'fubar')
   end
 
-  # - - - - - - - - - - - - - - - - -
-
   test 'E2A',
   'dispatch raises 400 when method name is unknown' do
-    @differ_stub = Object.new
+    @differ = Object.new
     assert_dispatch_raises('xyz', {}.to_json, 400, 'unknown path')
   end
 
+  test 'E2B',
+  'dispatch returns 400 status when JSON is malformed' do
+    @differ = Object.new
+    assert_dispatch_raises('xyz', [].to_json, 400, 'body is not JSON Hash')
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  class DifferStub
+    def sha
+      'hello from sha stub'
+    end
+    def ready?
+      'hello from ready? stub'
+    end
+  end
+
+  test 'E40',
+  'dispatch to ready? is 200' do
+    @differ = DifferStub.new
+    assert_dispatch('ready', {}.to_json, 'hello from ready? stub')
+  end
+
+  test 'E41',
+  'dispatch to sha is 200' do
+    @differ = DifferStub.new
+    assert_dispatch('sha', {}.to_json, 'hello from sha stub')
+  end
 
   private
 
@@ -69,11 +93,43 @@ class RackDispatcherTest < DifferTestBase
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+  def assert_dispatch(name, args, stubbed)
+    if query?(name)
+      qname = name + '?'
+    else
+      qname = name
+    end
+    assert_rack_call(name, args, { qname => stubbed })
+  end
+
+  def query?(name)
+    ['ready'].include?(name)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def assert_rack_call(name, args, expected)
+    response = rack_call(name, args)
+    assert_equal 200, response[0]
+    assert_equal({ 'Content-Type' => 'application/json' }, response[1])
+    assert_equal [to_json(expected)], response[2], args
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  def to_json(body)
+    JSON.generate(body)
+  end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
+
   def rack_call(name, args)
-    rack = RackDispatcher.new(@differ_stub, RackRequestStub)
+    rack = RackDispatcher.new(@differ, RackRequestStub)
     env = { path_info:name, body:args }
     rack.call(env)
   end
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - -
 
   def with_captured_stderr
     old_stderr = $stderr
