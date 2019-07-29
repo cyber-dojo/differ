3fe736512b0e8b3cf549a1ba366412af31443567
JonJagger <jon@jaggersoft.com>
2019-07-29 06:49:52 +0100

    tag locally built differ image so you can more easily run local integration tests

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -9,9 +9,15 @@ build_service_image()
   echo
   docker-compose \
     --file "${ROOT_DIR}/docker-compose.yml" \
-    build \
-      "${1}"
+      build \
+        "${1}"
 }
 
 build_service_image differ-server
 build_service_image differ-client
+
+# Assuming we do not have any new differ commits, differ's latest commit
+# sha will match the image tag inside versioner's .env file.
+# This means we can tag to it and a [cyber-dojo up] call
+# will use the tagged image.
+docker tag cyberdojo/differ:latest cyberdojo/differ:${SHA:0:7}
