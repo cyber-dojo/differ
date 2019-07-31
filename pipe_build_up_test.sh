a3607a092094718508d8723704e350abe38ec810
JonJagger <jon@jaggersoft.com>
2019-07-31 10:19:29 +0100

    refactoring; tidy pipe-build-up-test script so it leaves containers running if tests fail

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -13,7 +13,11 @@ fi
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
-"${SH_DIR}/build_docker_images.sh" "$@"
-"${SH_DIR}/docker_containers_up.sh" "$@"
-"${SH_DIR}/run_tests_in_containers.sh" "$@"
-"${SH_DIR}/docker_containers_down.sh"
+${SH_DIR}/build_docker_images.sh "$@"
+${SH_DIR}/docker_containers_up.sh "$@"
+if ${SH_DIR}/run_tests_in_containers.sh "$@" ; then
+  ${SH_DIR}/docker_containers_down.sh
+  exit 0
+else
+  exit 3
+fi
