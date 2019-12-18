46e5ddd0261979b92c0b35847d66db6ac83f16a3
Jon Jagger <jon@jaggersoft.com>
2019-12-18 16:02:19 +0000

    Move demo to root dir

--- run_demo.sh
+++ run_demo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
+readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
 ${SH_DIR}/build_docker_images.sh
 ${SH_DIR}/docker_containers_up.sh
