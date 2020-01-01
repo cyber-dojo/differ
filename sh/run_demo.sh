94164ac963ff3d008d6d005d13b535450d7a7beb
Jon Jagger <jon@jaggersoft.com>
2020-01-01 07:15:26 +0000

    Update demo script

--- sh/run_demo.sh
+++ sh/run_demo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -Ee
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-source ${SH_DIR}/cat_env_vars.sh
-export $(cat_env_vars)
+source ${SH_DIR}/versioner_env_vars.sh
+export $(versioner_env_vars)
 ${SH_DIR}/build_images.sh
 ${SH_DIR}/containers_up.sh
 
