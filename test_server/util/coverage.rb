63b5621ae7e8eac90eec65b063e96e6e435bf2be
Jon Jagger <jon@jaggersoft.com>
2020-01-01 19:11:58 +0000

    Update server coverage code

--- test_server/util/coverage.rb
+++ test_server/util/coverage.rb
@@ -1,9 +1,23 @@
 require 'simplecov'
 
-cov_root = File.expand_path('../..', __dir__)
-SimpleCov.start do
-  add_group( 'src') { | src|  src.filename.start_with?("#{cov_root}/src" ) }
-  add_group('test') { |test| test.filename.start_with?("#{cov_root}/test") }
+def app_root
+  File.expand_path('../..', __dir__)
 end
-SimpleCov.root(cov_root)
+
+def app_file?(filename)
+  filename.start_with?("#{app_root}/src/" )
+end
+
+def test_file?(filename)
+  filename.start_with?("#{app_root}/test/") &&
+    filename.end_with?('_test.rb')
+end
+
+SimpleCov.start do
+  #add_group('debug') {|src| puts src.filename; false; }
+  add_group('app') { |src| app_file?(src.filename) }
+  add_group('test') { |src| test_file?(src.filename) }
+end
+
+SimpleCov.root(app_root)
 SimpleCov.coverage_dir(ENV['COVERAGE_ROOT'])
