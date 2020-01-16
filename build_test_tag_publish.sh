d5ba5fbca6d9e24bd0d18072cac6f41cd2b33ee1
Jon Jagger <jon@jaggersoft.com>
2020-01-16 12:40:38 +0000

    [ci skip] Change script name to build_docker_images.sh as required by cyber-dojo/cyber-dojo script

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -13,7 +13,7 @@ fi
 readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 source ${SH_DIR}/versioner_env_vars.sh
 export $(versioner_env_vars)
-${SH_DIR}/build_images.sh "$@"
+${SH_DIR}/build_docker_images.sh "$@"
 ${SH_DIR}/containers_up.sh "$@"
 ${SH_DIR}/test_in_containers.sh "$@"
 ${SH_DIR}/containers_down.sh
