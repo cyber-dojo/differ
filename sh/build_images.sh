cd85adb6167f4506c5c77be031990995bc397994
Jon Jagger <jon@jaggersoft.com>
2020-02-13 16:45:26 +0000

    Refactoring; use port env-vars in Dockerfiles

--- sh/build_images.sh
+++ sh/build_images.sh
@@ -5,6 +5,7 @@ readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 # - - - - - - - - - - - - - - - - - - - - - -
 build_images()
 {
+  export COMMIT_SHA="$(git_commit_sha)"
   docker-compose \
     --file "${ROOT_DIR}/docker-compose.yml" \
     build \
