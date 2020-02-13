0508996736a94cb6cfbd5305247de4a5f5c2ba43
Jon Jagger <jon@jaggersoft.com>
2020-02-13 16:40:06 +0000

    Refactoring; put docker-compose.yml entries in alphabetical order

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -Ee
+#!/bin/bash -Eeu
 
-if [ "${1}" == '-h' ] || [ "${1}" == '--help' ]; then
+if [ "${1:-}" == '-h' ] || [ "${1:-}" == '--help' ]; then
   echo
   echo 'Use: pipe_build_up_test.sh [client|server] [HEX-ID...]'
   echo 'Options:'
@@ -13,7 +13,7 @@ fi
 readonly SH_DIR="$( cd "$( dirname "${0}" )/sh" && pwd )"
 source ${SH_DIR}/versioner_env_vars.sh
 export $(versioner_env_vars)
-${SH_DIR}/build_docker_images.sh "$@"
+${SH_DIR}/build_images.sh "$@"
 ${SH_DIR}/containers_up.sh "$@"
 ${SH_DIR}/test_in_containers.sh "$@"
 ${SH_DIR}/containers_down.sh
