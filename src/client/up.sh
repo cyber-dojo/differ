64fbd403c7fd48ae9a63c6b225285e550c0f62f3
Jon Jagger <jon@jaggersoft.com>
2020-02-26 08:50:21 +0000

    Set --server=thin explicitly in up.sh files; use MY_DIR for path to config.ru

--- src/client/up.sh
+++ src/client/up.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -Eeu
 
+readonly MY_DIR="$(cd "$(dirname "${0}")" && pwd)"
+
 export RUBYOPT='-W2'
 
 rackup \
   --env production \
   --port ${PORT}   \
+  --server thin    \
   --warn           \
-    /app/config.ru
+    ${MY_DIR}/config.ru
