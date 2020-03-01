f25a112fa518ee656a76405a4ec7ecd957e98e29
Jon Jagger <jon@jaggersoft.com>
2020-03-01 05:54:59 +0000

    Refactoring; script tidying

--- build_test_tag_publish.sh
+++ build_test_tag_publish.sh
@@ -1,27 +1,37 @@
 #!/bin/bash -Eeu
-
 readonly MY_NAME=`basename "${0}"`
-
-if [ "${1:-}" == '-h' ] || [ "${1:-}" == '--help' ]; then
-  echo
-  echo "Use: ${MY_NAME} [client|server] [ID...]"
-  echo 'Options:'
-  echo '   client  - only run the tests from inside the client'
-  echo '   server  - only run the tests from inside the server'
-  echo '   ID...   - only run the tests matching these identifiers'
-  echo
-  exit 0
-fi
-
 readonly SH_DIR="$(cd "$(dirname "${0}")/sh" && pwd)"
 source ${SH_DIR}/versioner_env_vars.sh
 export $(versioner_env_vars)
-${SH_DIR}/build_images.sh "$@"
-${SH_DIR}/containers_up.sh "$@"
-${SH_DIR}/test_in_containers.sh \
-  "${CYBER_DOJO_DIFFER_CLIENT_USER}" \
-  "${CYBER_DOJO_DIFFER_SERVER_USER}" \
-  "$@"
-${SH_DIR}/containers_down.sh
-${SH_DIR}/tag_image.sh
-${SH_DIR}/on_ci_publish_images.sh
+
+#- - - - - - - - - - - - - - - - - - - - - -
+main()
+{
+  local -r server_user="${CYBER_DOJO_DIFFER_CLIENT_USER}"
+  local -r client_user="${CYBER_DOJO_DIFFER_SERVER_USER}"
+  show_help_if_requested "$@"
+  ${SH_DIR}/build_images.sh "$@"
+  ${SH_DIR}/containers_up.sh "$@"
+  ${SH_DIR}/test_in_containers.sh "${server_user}" "${client_user}" "$@"
+  ${SH_DIR}/containers_down.sh
+  ${SH_DIR}/tag_image.sh
+  ${SH_DIR}/on_ci_publish_images.sh
+}
+
+#- - - - - - - - - - - - - - - - - - - - - -
+show_help_if_requested()
+{
+  if [ "${1:-}" == '-h' ] || [ "${1:-}" == '--help' ]; then
+    echo
+    echo "Use: ${MY_NAME} [client|server] [ID...]"
+    echo 'Options:'
+    echo '   client  - only run the tests from inside the client'
+    echo '   server  - only run the tests from inside the server'
+    echo '   ID...   - only run the tests matching these identifiers'
+    echo
+    exit 0
+  fi
+}
+
+#- - - - - - - - - - - - - - - - - - - - - -
+main "$@"
