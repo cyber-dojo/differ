50f2f84a02e32c732d9cc0c58a7df0974b9c27da
Jon Jagger <jon@jaggersoft.com>
2020-03-19 15:54:11 +0000

    Update coverage and test scripts

--- sh/test_in_containers.sh
+++ sh/test_in_containers.sh
@@ -7,6 +7,9 @@ readonly server_user="${1}"; shift
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 main()
 {
+  if on_ci; then
+    docker pull cyberdojo/check-test-results:latest
+  fi
   if [ "${1:-}" == 'client' ]; then
     shift
     run_client_tests "${@:-}"
@@ -14,11 +17,14 @@ main()
     shift
     run_server_tests "${@:-}"
   else
-    run_client_tests "${@:-}"
     run_server_tests "${@:-}"
+    run_client_tests "${@:-}"
   fi
   echo All passed
 }
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+on_ci() { [ -n "${CIRCLECI:-}" ]; }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
 run_client_tests() { run_tests "${client_user}" client "${@:-}"; }
@@ -29,12 +35,15 @@ run_tests()
 {
   local -r user="${1}" # eg nobody
   local -r type="${2}" # eg client|server
-  local -r reports_dir=reports
-  local -r coverage_root=/tmp/${reports_dir}
+  local -r reports_dir_name=reports
+  local -r tmp_dir=/tmp
+  local -r coverage_root=/${tmp_dir}/${reports_dir_name}
+  local -r test_dir="${root_dir}/test/${type}"
+  local -r reports_dir=${test_dir}/${reports_dir_name}
   local -r test_log=test.log
-  local -r container_name="test-${my_name}-${type}" # eg test-ragger-server
-  local -r coverage_code_group_name=tested
-  local -r coverage_test_group_name=tester
+  local -r container_name="test-${my_name}-${type}" # eg test-creator-server
+  local -r coverage_code_tab_name=tested
+  local -r coverage_test_tab_name=tester
 
   echo
   echo '=================================='
@@ -43,15 +52,14 @@ run_tests()
 
   set +e
   docker exec \
-    --env COVERAGE_CODE_GROUP_NAME=${coverage_code_group_name} \
-    --env COVERAGE_TEST_GROUP_NAME=${coverage_test_group_name} \
+    --env COVERAGE_CODE_TAB_NAME=${coverage_code_tab_name} \
+    --env COVERAGE_TEST_TAB_NAME=${coverage_test_tab_name} \
     --user "${user}" \
     "${container_name}" \
       sh -c "/test/run.sh ${coverage_root} ${test_log} ${type} ${*:3}"
   set -e
 
-  # You can't [docker cp] from a tmpfs, so tar-piping coverage out...
-  local -r test_dir="${root_dir}/test/${type}" # ...to this dir
+  # You can't [docker cp] from a tmpfs, so tar-piping coverage out
   docker exec \
     "${container_name}" \
     tar Ccf \
@@ -60,20 +68,21 @@ run_tests()
         | tar Cxf "${test_dir}/" -
 
   set +e
-  local -r data_dir=/tmp
-  docker run --rm \
-    --env COVERAGE_CODE_GROUP_NAME=${coverage_code_group_name} \
-    --env COVERAGE_TEST_GROUP_NAME=${coverage_test_group_name} \
-    --volume ${test_dir}/${reports_dir}:${data_dir}:ro \
+  docker run \
+    --env COVERAGE_CODE_TAB_NAME=${coverage_code_tab_name} \
+    --env COVERAGE_TEST_TAB_NAME=${coverage_test_tab_name} \
+    --rm \
+    --volume ${reports_dir}/${test_log}:${tmp_dir}/${test_log}:ro \
+    --volume ${reports_dir}/index.html:${tmp_dir}/index.html:ro \
     --volume ${test_dir}/metrics.rb:/app/metrics.rb:ro \
     cyberdojo/check-test-results:latest \
-    sh -c "ruby /app/check_test_results.rb ${data_dir}/${test_log} ${data_dir}/index.html" \
-      | tee -a ${test_dir}/${reports_dir}/${test_log}
+    sh -c "ruby /app/check_test_results.rb ${tmp_dir}/${test_log} ${tmp_dir}/index.html" \
+      | tee -a ${reports_dir}/${test_log}
   local -r status=${PIPESTATUS[0]}
   set -e
 
-  local -r coverage_path="test/${type}/${reports_dir}/index.html"
-  echo "Coverage files copied to ${coverage_path}"
+  local -r coverage_path="${reports_dir}/index.html"
+  echo "${type} test coverage at ${coverage_path}"
   echo "${type} test status == ${status}"
   if [ "${status}" != '0' ]; then
     docker logs "${container_name}"
--- test/coverage.rb
+++ test/coverage.rb
@@ -5,10 +5,8 @@ SimpleCov.start do
   filters.clear
   coverage_dir(ENV['COVERAGE_ROOT'])
   #add_group('debug') { |src| puts src.filename; false }
-  add_group(ENV['COVERAGE_CODE_GROUP_NAME']) { |src|
-    src.filename =~ %r"^/app/"
-  }
-  add_group(ENV['COVERAGE_TEST_GROUP_NAME']) { |src|
-    src.filename =~ %r"^/test/.*_test\.rb$"
-  }
+  code_tab = ENV['COVERAGE_CODE_TAB_NAME']
+  test_tab = ENV['COVERAGE_TEST_TAB_NAME']
+  add_group(code_tab) { |src| src.filename =~ %r"^/app/" }
+  add_group(test_tab) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
 end
