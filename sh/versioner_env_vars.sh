3833deff41f361c8d7d6392fa195158978e23316
Jon Jagger <jon@jaggersoft.com>
2020-08-21 12:58:56 +0100

    Fix bash script which has sha/tag swapped over

--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -3,24 +3,24 @@
 readonly ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-image_tag()
+image_sha()
 {
   echo "$(cd "${ROOT_DIR}" && git rev-parse HEAD)"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-image_sha()
+image_tag()
 {
-  local -r tag="$(image_tag)"
-  echo "${tag:0:7}"
+  local -r sha="$(image_sha)"
+  echo "${sha:0:7}"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 versioner_env_vars()
 {
   docker run --rm cyberdojo/versioner
-  echo CYBER_DOJO_DIFFER_TAG="$(image_tag)"
   echo CYBER_DOJO_DIFFER_SHA="$(image_sha)"
+  echo CYBER_DOJO_DIFFER_TAG="$(image_tag)"
   echo CYBER_DOJO_DIFFER_CLIENT_PORT=9999
   echo CYBER_DOJO_DIFFER_CLIENT_USER=nobody
   echo CYBER_DOJO_DIFFER_SERVER_USER=nobody
