300fa2169d5087f1f23e8abd95b202102f30f459
Jon Jagger <jon@jaggersoft.com>
2020-08-22 08:38:51 +0100

    Fix bash script function name

--- sh/on_ci_publish_images.sh
+++ sh/on_ci_publish_images.sh
@@ -7,29 +7,19 @@ on_ci()
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
-image_name()
-{
-  echo "${CYBER_DOJO_DIFFER_IMAGE}"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-image_tag()
-{
-  echo "${CYBER_DOJO_DIFFER_TAG}"
-}
-
-# - - - - - - - - - - - - - - - - - - - - - - - -
-on_ci_publish_tagged_images()
+on_ci_publish_images()
 {
   if ! on_ci; then
     echo 'not on CI so not publishing tagged images'
-    return
+  else
+    echo 'on CI so publishing tagged images'
+    local -r image_name="${CYBER_DOJO_DIFFER_IMAGE}"
+    local -r image_tag="${CYBER_DOJO_DIFFER_TAG}"
+    # DOCKER_USER, DOCKER_PASS are in ci context
+    echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+    docker tag ${image_name}:${image_tag} ${image_name}:latest
+    docker push ${image_name}:${image_tag}
+    docker push ${image_name}:latest
+    docker logout
   fi
-  echo 'on CI so publishing tagged images'
-  # DOCKER_USER, DOCKER_PASS are in ci context
-  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
-  docker tag $(image_name):$(image_tag) $(image_name):latest
-  docker push $(image_name):$(image_tag)
-  docker push $(image_name):latest
-  docker logout
 }
