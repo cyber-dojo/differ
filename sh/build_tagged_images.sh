33577af12e2943e4addc2ecef17f0d8113c25b72
Jon Jagger <jon@jaggersoft.com>
2020-08-22 09:48:08 +0100

    Delete old image before creating new one

--- sh/build_tagged_images.sh
+++ sh/build_tagged_images.sh
@@ -3,17 +3,44 @@
 # - - - - - - - - - - - - - - - - - - - - - -
 build_tagged_images()
 {
+  remove_current_docker_image "${CYBER_DOJO_DIFFER_IMAGE}"
+  remove_current_docker_image "${CYBER_DOJO_DIFFER_CLIENT_IMAGE}"
+
   docker-compose \
     --file "${SH_DIR}/../docker-compose.yml" \
     build \
     --build-arg COMMIT_SHA=$(git_commit_sha)
 
   docker tag $(image_name):$(image_tag) $(image_name):latest
+  docker tag ${CYBER_DOJO_DIFFER_CLIENT_IMAGE}:$(image_tag) ${CYBER_DOJO_DIFFER_CLIENT_IMAGE}:latest
 
   check_embedded_env_var
   echo
   echo "CYBER_DOJO_DIFFER_TAG=$(image_tag)"
   echo "CYBER_DOJO_DIFFER_SHA=$(image_sha)"
+}
+
+# - - - - - - - - - - - - - - - - - - - - - -
+remove_current_docker_image()
+{
+  local -r name="${1}"
+  if image_exists "${name}" 'latest' ; then
+    local -r sha="$(docker run --rm -it ${name}:latest sh -c 'echo -n ${SHA}')"
+    local -r tag="${sha:0:7}"
+    if image_exists "${name}" "${tag}" ; then
+      echo "Deleting current image ${name}:${tag}"
+      docker image rm "${name}:${tag}"
+    fi
+  fi
+}
+
+# - - - - - - - - - - - - - - - - - - - - - -
+image_exists()
+{
+  local -r name="${1}"
+  local -r tag="${2}"
+  local -r latest=$(docker image ls --format "{{.Repository}}:{{.Tag}}" | grep "${name}:${tag}")
+  [ "${latest}" != '' ]
 }
 
 # - - - - - - - - - - - - - - - - - - - - - -
--- sh/versioner_env_vars.sh
+++ sh/versioner_env_vars.sh
@@ -21,6 +21,7 @@ versioner_env_vars()
   docker run --rm cyberdojo/versioner
   echo CYBER_DOJO_DIFFER_SHA="$(image_sha)"
   echo CYBER_DOJO_DIFFER_TAG="$(image_tag)"
+  echo CYBER_DOJO_DIFFER_CLIENT_IMAGE=cyberdojo/differ-client
   echo CYBER_DOJO_DIFFER_CLIENT_PORT=9999
   echo CYBER_DOJO_DIFFER_CLIENT_USER=nobody
   echo CYBER_DOJO_DIFFER_SERVER_USER=nobody
