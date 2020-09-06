3876ef4494aa1acddc164f1805f490589e91de77
Jon Jagger <jon@jaggersoft.com>
2020-09-06 18:37:18 +0100

    Still trying to set k8s helm env-var

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -5,7 +5,6 @@ readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
 readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
 source <(curl "${K8S_URL}/sh/deployment_functions.sh")
 export $(curl "${VERSIONER_URL}/app/.env")
-export CYBER_DOJO_PROMETHEUS=true
 readonly CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
 
 gcloud_init
