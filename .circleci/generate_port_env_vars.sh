619b80d80acfbe2391ca4e24dc790e1c500e7cb2
Jon Jagger <jon@jaggersoft.com>
2020-09-07 16:40:53 +0100

    Trying to generate valid yml in bash script

--- .circleci/generate_port_env_vars.sh
+++ .circleci/generate_port_env_vars.sh
@@ -3,7 +3,7 @@ generate_port_env_vars()
 {
   echo
   echo 'containers:'
-  echo '   env:'
+  echo '  - env:'
   for line in $(docker run --rm cyberdojo/versioner | grep PORT | tr ' ' '\n')
   do
     name="${line%=*}"
