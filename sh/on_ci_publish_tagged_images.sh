935f3fa9df60a939fc6f94cd9133f792e3fb9e06
Jon Jagger <jon@jaggersoft.com>
2020-10-08 13:41:31 +0100

    Do non-anonymous DockerHub pulls on CI (anonymous pulls have stricter pull limits)

--- sh/on_ci_publish_tagged_images.sh
+++ sh/on_ci_publish_tagged_images.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -Ee
+#!/bin/bash -Eeu
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
 on_ci_publish_tagged_images()
@@ -9,11 +9,8 @@ on_ci_publish_tagged_images()
     echo 'on CI so publishing tagged images'
     local -r name="${CYBER_DOJO_DIFFER_IMAGE}"
     local -r tag="${CYBER_DOJO_DIFFER_TAG}"
-    # DOCKER_USER, DOCKER_PASS are in ci context
-    echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
     docker push ${name}:${tag}
     docker push ${name}:latest
-    docker logout
   fi
 }
 
