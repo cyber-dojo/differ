3b9e87530c10d14da620fc2404894ff3ec9977c0
Jon Jagger <jon@jaggersoft.com>
2020-10-11 09:52:12 +0100

    Give client and server tests their own lib/ dir

--- test/client/lib/run.sh
+++ test/client/lib/run.sh
@@ -6,7 +6,7 @@ readonly TEST_LOG="${2}"    # test.log
 readonly TYPE="${3}"        # client|server
 shift; shift; shift
 
-readonly TEST_FILES=(${MY_DIR}/../${TYPE}/*_test.rb)
+readonly TEST_FILES=(${MY_DIR}/../*_test.rb)
 readonly TEST_ARGS=(${@})
 
 readonly SCRIPT="
---
+++ test/server/lib/coverage.rb
@@ -0,0 +1,12 @@
+require 'simplecov'
+
+SimpleCov.start do
+  #enable_coverage :branch
+  filters.clear
+  coverage_dir(ENV['COVERAGE_ROOT'])
+  #add_group('debug') { |src| puts src.filename; false }
+  code_tab = ENV['COVERAGE_CODE_TAB_NAME']
+  test_tab = ENV['COVERAGE_TEST_TAB_NAME']
+  add_group(code_tab) { |src| src.filename =~ %r"^/differ/" }
+  add_group(test_tab) { |src| src.filename =~ %r"^/test/.*_test\.rb$" }
+end
