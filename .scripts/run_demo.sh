78d959d8f06778b529d9e158c199cb061e2a8482
Jon Jagger <jon@jaggersoft.com>
2020-10-11 14:32:45 +0100

    [ci skip] Resurrect client-side demo

--- .scripts/run_demo.sh
+++ .scripts/run_demo.sh
@@ -1,16 +1,22 @@
-#!/bin/bash -Ee
+#!/bin/bash -Eeu
 
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+export SH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly SH_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source ${SH_DIR}/versioner_env_vars.sh
+source "${SH_DIR}/build_tagged_images.sh"
+source "${SH_DIR}/containers_up.sh"
+source "${SH_DIR}/versioner_env_vars.sh"
 export $(versioner_env_vars)
-${SH_DIR}/build_images.sh
-${SH_DIR}/containers_up.sh
 
-TMP_HTML_FILENAME=/tmp/differ-demo.html
+readonly TMP_HTML_FILENAME=/tmp/differ-demo.html
+
+build_tagged_images
+
+containers_up
 
 docker exec \
   test-differ-client \
-    sh -c 'ruby /app/src/html_demo.rb' \
+    sh -c 'ruby /differ/app/html_demo.rb' \
       > ${TMP_HTML_FILENAME}
 
 open "file://${TMP_HTML_FILENAME}"
