ff84ba927d64f679c6ca072317307dbf6789d52d
Jon Jagger <jon@jaggersoft.com>
2020-10-21 10:29:00 +0100

    Added script function to copy saver-test-data into saver container

--- scripts/containers_up.sh
+++ scripts/containers_up.sh
@@ -149,5 +149,17 @@ containers_up()
   else
     container_up_ready_and_clean "${CYBER_DOJO_DIFFER_CLIENT_PORT}" differ-client
   fi
-  #curl --silent --fail -X GET http://${IP_ADDRESS}:${CYBER_DOJO_DIFFER_PORT}/sha
+  copy_in_saver_test_data
+}
+
+# - - - - - - - - - - - - - - - - - - -
+copy_in_saver_test_data()
+{
+  local -r SRC_PATH=${ROOT_DIR}/test/data/cyber-dojo
+  local -r SAVER_CID=$(docker ps --filter status=running --format '{{.Names}}' | grep "saver")
+  local -r DEST_PATH=/cyber-dojo
+  # You cannot docker cp to a tmpfs, so tar-piping instead...
+  cd ${SRC_PATH} \
+    && tar -c . \
+    | docker exec -i ${SAVER_CID} tar x -C ${DEST_PATH}
 }
