1d07c2710e824546870ae5541c98d4bf33921462
Jon Jagger <jon@jaggersoft.com>
2020-10-28 11:27:59 +0000

    Tidied bash script

--- app/rack_dispatcher.rb
+++ app/rack_dispatcher.rb
@@ -17,31 +17,32 @@ class RackDispatcher
     params = request.params
     body = request.body.read
     result = HttpJsonArgs::dispatch(path, @differ, body, params)
-    json_response_pass(200, result)
+    html_json_pass(200, result)
   rescue HttpJsonArgs::RequestError => caught
-    json_response_fail(400, path, params, body, caught)
+    html_json_fail(400, path, params, body, caught)
   rescue Exception => caught
-    json_response_fail(500, path, params, body, caught)
+    html_json_fail(500, path, params, body, caught)
   end
 
   private
 
-  def json_response_pass(status, result)
-    body = JSON.fast_generate(result)
-    [ status, CONTENT_TYPE_JSON, [body] ]
+  def html_json_pass(status, result)
+    html_json(status, JSON.fast_generate(result))
   end
 
-  # - - - - - - - - - - - - - - - -
-
-  def json_response_fail(status, path, params, body, caught)
-    body = JSON.pretty_generate(diagnostic(path, params, body, caught))
+  def html_json_fail(status, path, params, body, caught)
+    json = JSON.pretty_generate(diagnostic(path, params, body, caught))
     if ['/alive','/ready'].include?(path)
-      IO.write("/tmp#{path}.fail.log", body)
+      IO.write("/tmp#{path}.fail.log", json)
     else
-      $stderr.puts(body)
+      $stderr.puts(json)
       $stderr.flush
     end
-    [ status, CONTENT_TYPE_JSON, [body] ]
+    html_json(status, json)
+  end
+
+  def html_json(status, json)
+    [ status, { 'Content-Type' => 'application/json' }, [json] ]
   end
 
   # - - - - - - - - - - - - - - - -
@@ -58,9 +59,5 @@ class RackDispatcher
       }
     }
   end
-
-  # - - - - - - - - - - - - - - - -
-
-  CONTENT_TYPE_JSON = { 'Content-Type' => 'application/json' }
 
 end
