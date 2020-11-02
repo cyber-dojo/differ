cddbc35b93e5effb05b0d45014ec7973c714d9af
Jon Jagger <jon@jaggersoft.com>
2020-11-02 19:44:16 +0000

    Preparing diff_lines(indexes) to replace diff(files)

--- app/differ.rb
+++ app/differ.rb
@@ -2,7 +2,7 @@
 require_relative 'git_differ'
 require_relative 'git_diff_join'
 require_relative 'git_diff_lib'
-#require_relative 'git_diff_lines'
+require_relative 'git_diff_lines'
 require_relative 'git_diff_parser'
 require_relative 'git_diff_summary'
 require_relative 'prober'
@@ -25,17 +25,36 @@ class Differ
   end
 
 =begin
+  def diff_lines(id:, was_index:, now_index:)
+    # args from request query will be strings
+    was = model.kata_event(id, was_index.to_i)
+    now = model.kata_event(id, now_index.to_i)
+    was_files = files(was)
+    now_files = files(now)
+    diff_lines = GitDiffer.new(@externals).diff(id, was_files, now_files)
+    diffs = GitDiffParser.new(diff_lines, lines:true, counts:true).parse_all
+    { 'diff_lines' => git_diff_lines(diffs, now_files),
+      'stdout' => now['stdout'],
+      'stderr' => now['stderr'],
+      'status' => now['status']
+    }
+  end
+=end
+
+=begin
   def diff_lines2(id:, old_files:, new_files:)
     diff_lines = GitDiffer.new(@externals).diff(id, old_files, new_files)
-    diffs = GitDiffParser.new(diff_lines, :both).parse_all
+    diffs = GitDiffParser.new(diff_lines, lines:true, counts:true).parse_all
     { 'diff_lines2' => git_diff_lines(diffs, new_files) }
   end
 =end
 
   def diff_summary2(id:, was_index:, now_index:)
     # args from request query will be strings
-    was_files = model_files(id, was_index.to_i)
-    now_files = model_files(id, now_index.to_i)
+    was = model.kata_event(id, was_index.to_i)
+    now = model.kata_event(id, now_index.to_i)
+    was_files = files(was)
+    now_files = files(now)
     diff_lines = GitDiffer.new(@externals).diff(id, was_files, now_files)
     diffs = GitDiffParser.new(diff_lines, counts:true).parse_all
     { 'diff_summary2' => git_diff_summary(diffs, now_files) }
@@ -47,8 +66,8 @@ class Differ
 
   # - - - - - - - - - - - - -
 
-  def model_files(id, index)
-    model.kata_event(id, index)['files'].map{|filename, file|
+  def files(event)
+    event['files'].map{ |filename, file|
       [filename, file['content']]
     }.to_h
   end
