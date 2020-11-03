302597500ba052f2f0d98fa8e59798a6eab27a52
Jon Jagger <jon@jaggersoft.com>
2020-11-03 16:36:59 +0000

    [ci skip] Added comments.

--- app/differ.rb
+++ app/differ.rb
@@ -18,39 +18,38 @@ class Differ
   def alive?  ; prober.alive?  ; end
   def ready?  ; prober.ready?  ; end
 
-  def diff(id:, old_files:, new_files:)
+  def diff(id:, old_files:, new_files:) # TODO: Delete
     git_diff = GitDiffer.new(@externals).diff(id, old_files, new_files)
     result = git_diff_join(git_diff, old_files, new_files)
     { 'diff' => result }
   end
 
+  #=begin
+    def diff_lines2(id:, old_files:, new_files:)
+      diff_lines = GitDiffer.new(@externals).diff(id, old_files, new_files)
+      diffs = GitDiffParser.new(diff_lines, lines:true, counts:true).parse_all
+      { 'diff_lines2' => git_diff_lines(diffs, new_files) }
+    end
+  #=end
+
 =begin
   def diff_lines(id:, was_index:, now_index:)
-    # args from request query will be strings
     was = model.kata_event(id, was_index.to_i)
     now = model.kata_event(id, now_index.to_i)
     was_files = files(was)
     now_files = files(now)
+    # Ensure stdout/stderr/status show no diff. Drop once web's
+    # review handles stdout/stderr/status separately as true files.
+    was_files['stdout'] = now_files['stdout'] = now['stdout']['content']
+    was_files['stderr'] = now_files['stderr'] = now['stderr']['content']
+    was_files['status'] = now_files['status'] = now['status']
     diff_lines = GitDiffer.new(@externals).diff(id, was_files, now_files)
     diffs = GitDiffParser.new(diff_lines, lines:true, counts:true).parse_all
-    { 'diff_lines' => git_diff_lines(diffs, now_files),
-      'stdout' => now['stdout'],
-      'stderr' => now['stderr'],
-      'status' => now['status']
-    }
+    git_diff_lines(diffs, now_files)
   end
 =end
 
-#=begin
-  def diff_lines2(id:, old_files:, new_files:)
-    diff_lines = GitDiffer.new(@externals).diff(id, old_files, new_files)
-    diffs = GitDiffParser.new(diff_lines, lines:true, counts:true).parse_all
-    { 'diff_lines2' => git_diff_lines(diffs, new_files) }
-  end
-#=end
-
   def diff_summary(id:, was_index:, now_index:)
-    # args from request query will be strings
     was = model.kata_event(id, was_index.to_i)
     now = model.kata_event(id, now_index.to_i)
     was_files = files(was)
