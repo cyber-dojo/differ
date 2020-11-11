affe451e00922f0012f561d0aff1a2d8542e69d7
Jon Jagger <jon@jaggersoft.com>
2020-11-11 19:12:00 +0000

    Restored http-proxy raising exception when received JSON-Hash has no key matching path.

--- app/http_json_hash/unpacker.rb
+++ app/http_json_hash/unpacker.rb
@@ -11,35 +11,31 @@ module HttpJsonHash
       @requester = requester
     end
 
-    # - - - - - - - - - - - - - - - - - - - - -
-
     def get(path, args)
       response = @requester.get(path, args)
       unpacked(response.body, path.to_s, args)
     end
 
-    # - - - - - - - - - - - - - - - - - - - - -
-
     private
 
     def unpacked(body, path, args)
       json = JSON.parse!(body)
-      if json.is_a?(Hash) && json.has_key?('exception')
-        service_error(path, args, body, 'body has embedded exception')
+      unless json.instance_of?(Hash)
+        fail service_error(path, args, body, 'body is not JSON Hash')
       end
-      if json.is_a?(Hash) && json.has_key?(path)
-        json[path]
-      else
-        json
+      if json.has_key?('exception')
+        fail service_error(path, args, body, 'body has embedded exception')
       end
+      unless json.has_key?(path)
+        fail service_error(path, args, body, 'body is missing :path key')
+      end
+      json[path]
     rescue JSON::ParserError
-      service_error(path, args, body, 'body is not JSON')
+      fail service_error(path, args, body, 'body is not JSON')
     end
 
-    # - - - - - - - - - - - - - - - - - - - - -
-
     def service_error(path, args, body, message)
-      fail ::HttpJsonHash::ServiceError.new(path, args, @name, body, message)
+      ::HttpJsonHash::ServiceError.new(path, args, @name, body, message)
     end
 
   end
--- test/http_proxy_response_test.rb
+++ test/http_proxy_response_test.rb
@@ -8,36 +8,71 @@ class HttpProxyResponseTest < DifferTestBase
     'f28'
   end
 
-  test 'QN7', %w(
-  |when an http-proxy response.body's JSON-Hash does not have a key matching the path
-  |then it returns the JSON-Hash
-  ) do
-    http = ::HttpAdapterStub.new('{"wibble":42}')
-    model = ::External::Model.new(http)
-    assert_equal({"wibble"=>42}, model.ready?)
-  end
+  # - - - - - - - - - - - - - - - - -
 
   test 'QN4', %w(
-  |when an http-proxy response.body is not JSON
-  |then an exception is raised
+  |when an http-proxy
+  |receives non-JSON in its response.body
+  |it raises an exception
   ) do
     stub_model_http('xxxx')
     ready_raises_exception
   end
 
+  # - - - - - - - - - - - - - - - - -
+
+  test 'QN5', %w(
+  |when an http-proxy
+  |receives JSON (but not a Hash) in its response.body
+  |it raises an exeption
+  ) do
+    stub_model_http('[]')
+    ready_raises_exception
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
   test 'QN6', %w(
-  |when an http-proxy response.body's JSON-Hash has a key 'exception'
-  |then it raises the exeption
+  |when an http-proxy
+  |receives JSON-Hash with an 'exception' key in its response.body
+  |it raises an exeption
   ) do
     stub_model_http(response='{"exception":42}')
     ready_raises_exception
   end
 
+  # - - - - - - - - - - - - - - - - -
+
+  test 'QN7', %w(
+  |when an http-proxy
+  |receives JSON-Hash in its response.body
+  |which does not contain the requested method's key
+  |it raises an exeption
+  ) do
+    stub_model_http(response='{"wibble":42}')
+    ready_raises_exception
+  end
+
+  # - - - - - - - - - - - - - - - - -
+
+  test 'QN8', %w(
+  |when an http-proxy
+  |receives a JSON-Hash in its response.body
+  |which has a key matching the path
+  |then it returns the value for that key
+  ) do
+    http = ::HttpAdapterStub.new('{"ready?":[42]}')
+    model = ::External::Model.new(http)
+    assert_equal [42], model.ready?
+  end
+
   private
 
   def stub_model_http(body)
-    externals.instance_exec { @model_http = ::HttpAdapterStub.new(body) }
+    externals.instance_exec { @model_http = HttpAdapterStub.new(body) }
   end
+
+  # - - - - - - - - - - - - - - - - -
 
   def ready_raises_exception
     assert_raises { prober.ready? }
