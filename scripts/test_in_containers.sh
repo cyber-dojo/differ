b5a886013c28fafece476b4d3877e32c43ae1209
Jon Jagger <jon@jaggersoft.com>
2020-12-26 10:53:41 +0000

    Refactoring; script tidying.

--- scripts/test_in_containers.sh
+++ scripts/test_in_containers.sh
@@ -40,18 +40,18 @@ run_tests()
   local -r CONTAINER_NAME="${2}" # eg test_differ_server
   local -r TYPE="${3}"           # eg server
 
-  local -r COVERAGE_CODE_TAB_NAME=app
-  local -r COVERAGE_TEST_TAB_NAME=test
-  local -r CONTAINER_TMP_DIR=/tmp
-  local -r CONTAINER_COVERAGE_DIR=/${CONTAINER_TMP_DIR}/reports
-  local -r TEST_LOG=test.log
-
   echo '=================================='
   echo "Running ${TYPE} tests"
   echo '=================================='
 
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Run tests (with coverage) inside the container.
+
+  local -r COVERAGE_CODE_TAB_NAME=app
+  local -r COVERAGE_TEST_TAB_NAME=test
+  local -r CONTAINER_TMP_DIR=/tmp # fs is read-only with tmpfs at /tmp
+  local -r CONTAINER_COVERAGE_DIR="/${CONTAINER_TMP_DIR}/reports"
+  local -r TEST_LOG=test.log
 
   set +e
   docker exec \
@@ -66,10 +66,10 @@ run_tests()
   # Extract test-run results and coverage data from the container.
   # You can't [docker cp] from a tmpfs, so tar-piping coverage out
 
-  if [ "${TYPE}" == 'server' ]; then
-    local -r HOST_TEST_DIR="${SH_DIR}/../test"
+  if [ "${TYPE}" == server ]; then
+    local -r HOST_TEST_DIR="${ROOT_DIR}/test"
   else
-    local -r HOST_TEST_DIR="${SH_DIR}/../client/test"
+    local -r HOST_TEST_DIR="${ROOT_DIR}/client/test"
   fi
 
   docker exec \
@@ -82,21 +82,26 @@ run_tests()
   #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # Process test-run results and coverage data.
 
-  local -r HOST_REPORTS_DIR=${HOST_TEST_DIR}/reports
+  local -r HOST_REPORTS_DIR="${HOST_TEST_DIR}/reports"
   mkdir -p "${HOST_REPORTS_DIR}"
 
   set +e
   docker run \
-    --env COVERAGE_CODE_TAB_NAME=${COVERAGE_CODE_TAB_NAME} \
-    --env COVERAGE_TEST_TAB_NAME=${COVERAGE_TEST_TAB_NAME} \
+    --env COVERAGE_CODE_TAB_NAME="${COVERAGE_CODE_TAB_NAME}" \
+    --env COVERAGE_TEST_TAB_NAME="${COVERAGE_TEST_TAB_NAME}" \
     --rm \
     --volume ${HOST_REPORTS_DIR}/${TEST_LOG}:${CONTAINER_TMP_DIR}/${TEST_LOG}:ro \
     --volume ${HOST_REPORTS_DIR}/index.html:${CONTAINER_TMP_DIR}/index.html:ro \
     --volume ${HOST_REPORTS_DIR}/coverage.json:${CONTAINER_TMP_DIR}/coverage.json:ro \
     --volume ${HOST_TEST_DIR}/lib/metrics.rb:/app/metrics.rb:ro \
     cyberdojo/check-test-results:latest \
-    sh -c "ruby /app/check_test_results.rb ${CONTAINER_TMP_DIR}/${TEST_LOG} ${CONTAINER_TMP_DIR}/index.html ${CONTAINER_TMP_DIR}/coverage.json" \
-      | tee -a ${HOST_REPORTS_DIR}/${TEST_LOG}
+      sh -c \
+        "ruby /app/check_test_results.rb \
+          ${CONTAINER_TMP_DIR}/${TEST_LOG} \
+          ${CONTAINER_TMP_DIR}/index.html \
+          ${CONTAINER_TMP_DIR}/coverage.json" \
+    | tee -a ${HOST_REPORTS_DIR}/${TEST_LOG}
+
   local -r STATUS=${PIPESTATUS[0]}
   set -e
 
@@ -106,7 +111,7 @@ run_tests()
   echo "${TYPE} test coverage at $(abs_filename "${HOST_REPORTS_DIR}/index.html")"
   echo "${TYPE} test status == ${STATUS}"
   echo
-  if [ "${STATUS}" != '0' ]; then
+  if [ "${STATUS}" != 0 ]; then
     echo Docker logs "${CONTAINER_NAME}"
     echo
     docker logs "${CONTAINER_NAME}" 2>&1
