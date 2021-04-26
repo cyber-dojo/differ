47ca72b0bb43d176ee012755e80e095167a0f9b5
Sami Alajrami <sami@merkely.com>
2021-04-26 12:19:42 +0200

    install pyhton as dependency

--- .circleci/connect-to-k8s.sh
+++ .circleci/connect-to-k8s.sh
@@ -2,6 +2,7 @@
 
 apk update
 apk add curl
+apk add python3
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-337.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-sdk-337.0.0-linux-x86_64.tar.gz
 cd google-cloud-sdk
