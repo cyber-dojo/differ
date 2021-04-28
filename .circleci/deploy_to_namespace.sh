15b1d3bca62f39433a9d9dc052dd74919afe612e
Jon Jagger <jon@jaggersoft.com>
2021-04-28 16:08:54 +0100

    Add missing command to source merkely script file

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,13 +1,15 @@
 #!/bin/bash -Eeu
 
-readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
 readonly NAMESPACE="${1}" # beta|prod
 readonly K8S_URL=https://raw.githubusercontent.com/cyber-dojo/k8s-install/master
 readonly VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
 source <(curl "${K8S_URL}/sh/deployment_functions.sh")
 export $(curl "${VERSIONER_URL}/app/.env")
 readonly CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
-readonly YAML_VALUES_FILE="${MY_DIR}/k8s-general-values.yml"
+readonly YAML_VALUES_FILENAME="${ROOT_DIR}/.circleci/k8s-general-values.yml"
+
 
 gcloud_init
 helm_init
@@ -18,6 +20,8 @@ helm_upgrade_probe_yes_prometheus_yes \
    "${CYBER_DOJO_DIFFER_IMAGE}" \
    "${CYBER_DOJO_DIFFER_TAG}" \
    "${CYBER_DOJO_DIFFER_PORT}" \
-   "${YAML_VALUES_FILE}"
+   "${YAML_VALUES_FILENAME}"
+
+source "${ROOT_DIR}/scripts/merkely.sh"
 
 on_ci_merkely_log_deployment "${NAMESPACE}"
