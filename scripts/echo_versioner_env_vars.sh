21999918bae5ebf70b5fecf9cd353db4df50eb18
Jon Jagger <jon@jaggersoft.com>
2021-05-12 19:06:34 +0100

    Prepare for saver/model upgrade

--- scripts/echo_versioner_env_vars.sh
+++ scripts/echo_versioner_env_vars.sh
@@ -15,4 +15,10 @@ echo_versioner_env_vars()
   #
   echo CYBER_DOJO_DIFFER_CLIENT_CONTAINER_NAME=test_differ_client
   echo CYBER_DOJO_DIFFER_SERVER_CONTAINER_NAME=test_differ_server
+
+  # Forthcoming deployments
+  #echo CYBER_DOJO_SAVER_SHA=2ae8e51362c5ad215b86d6065b0f850fae667ea8
+  #echo CYBER_DOJO_SAVER_TAG=2ae8e51
+  #echo CYBER_DOJO_MODEL_SHA=3fb3f3764cab60078fe5e4577a7a94b786cef308
+  #echo CYBER_DOJO_MODEL_TAG=3fb3f37
 }
