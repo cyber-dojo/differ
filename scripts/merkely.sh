a384b8f1665e2646d0b3b47526bea83ebf5b22a6
Jon Jagger <jon@jaggersoft.com>
2021-07-06 12:44:22 +0100

    Repeat all merkely log commands for staging and prod Merkely servers

--- scripts/merkely.sh
+++ scripts/merkely.sh
@@ -26,6 +26,16 @@ on_ci_merkely_declare_pipeline()
 		--rm \
 		--volume ${ROOT_DIR}/Merkelypipe.json:/data/Merkelypipe.json \
 		${MERKELY_CHANGE}
+
+	docker run \
+		--env MERKELY_COMMAND=declare_pipeline \
+    --env MERKELY_OWNER=${MERKELY_OWNER} \
+    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+		--env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --env MERKELY_HOST=https://staging.app.merkely.com \
+		--rm \
+		--volume ${ROOT_DIR}/Merkelypipe.json:/data/Merkelypipe.json \
+		${MERKELY_CHANGE}
 }
 
 # - - - - - - - - - - - - - - - - - - -
@@ -45,6 +55,22 @@ on_ci_merkely_log_artifact()
     --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --rm \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    ${MERKELY_CHANGE}
+
+	docker run \
+    --env MERKELY_COMMAND=log_artifact \
+    --env MERKELY_OWNER=${MERKELY_OWNER} \
+    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
+    --env MERKELY_IS_COMPLIANT=TRUE \
+    --env MERKELY_ARTIFACT_GIT_COMMIT=${CYBER_DOJO_DIFFER_SHA} \
+    --env MERKELY_ARTIFACT_GIT_URL=https://github.com/${MERKELY_OWNER}/${MERKELY_PIPELINE}/commit/${CYBER_DOJO_DIFFER_SHA} \
+    --env MERKELY_CI_BUILD_NUMBER=${CIRCLE_BUILD_NUM} \
+    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --env MERKELY_HOST=https://staging.app.merkely.com \
     --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     ${MERKELY_CHANGE}
@@ -68,6 +94,23 @@ on_ci_merkely_log_evidence()
     --env MERKELY_USER_DATA="$(evidence_json_path)" \
     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --rm \
+    --volume "$(evidence_json_path):$(evidence_json_path)" \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    ${MERKELY_CHANGE}
+
+	docker run \
+    --env MERKELY_COMMAND=log_evidence \
+    --env MERKELY_OWNER=${MERKELY_OWNER} \
+    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
+    --env MERKELY_EVIDENCE_TYPE=branch-coverage \
+    --env MERKELY_IS_COMPLIANT=TRUE \
+    --env MERKELY_DESCRIPTION="server & client branch-coverage reports" \
+    --env MERKELY_USER_DATA="$(evidence_json_path)" \
+    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --env MERKELY_HOST=https://staging.app.merkely.com \
     --rm \
     --volume "$(evidence_json_path):$(evidence_json_path)" \
     --volume /var/run/docker.sock:/var/run/docker.sock \
