3f747ca1fb3e399f7e14f521bd31622c3a375e39
Jon Jagger <jon@jaggersoft.com>
2021-07-06 12:38:13 +0100

    Log deployment to Merkely prod and to Merkely staging server

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -34,6 +34,20 @@ merkely_log_deployment()
     --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
     ${MERKELY_CHANGE}
+
+	docker run \
+    --env MERKELY_COMMAND=log_deployment \
+    --env MERKELY_OWNER=${MERKELY_OWNER} \
+    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+    --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
+    --env MERKELY_DESCRIPTION="Deployed to ${environment} in circleci pipeline" \
+    --env MERKELY_ENVIRONMENT="${environment}" \
+    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+    --env MERKELY_HOST=https://staging.app.merkely.com \
+    --rm \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    ${MERKELY_CHANGE}
 }
 
 merkely_log_deployment "${1}"
