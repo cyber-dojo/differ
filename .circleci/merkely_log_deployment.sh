7c952da58a60957c92eff351344648a8dd1bd353
Jon Jagger <jon@jaggersoft.com>
2021-08-22 19:27:26 +0100

    Force CI run

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -3,20 +3,20 @@
 # - - - - - - - - - - - - - - - - - - -
 merkely_log_deployment()
 {
-  local -r environment="${1}"
-  local -r hostname="${2}"
-
-  # set CYBER_DOJO_DIFFER_IMAGE, CYBER_DOJO_DIFFER_TAG
-  local -r VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
-  export $(curl "${VERSIONER_URL}/app/.env")
-  export CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
-
-  # get image so fingerprint() works
-  docker pull ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}
-
-  local -r MERKELY_CHANGE=merkely/change:latest
+  local -r MERKELY_ENVIRONMENT="${1}"
+  local -r MERKELY_HOST="${2}"
   local -r MERKELY_OWNER=cyber-dojo
   local -r MERKELY_PIPELINE=differ
+
+  # Set CYBER_DOJO_DIFFER_IMAGE, CYBER_DOJO_DIFFER_TAG
+  local -r VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
+  export $(curl "${VERSIONER_URL}/app/.env")
+  local -r CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
+
+  # Pull image so merkely_fingerprint() works
+  echo "${DOCKER_PASS}" | docker login --username "${DOCKER_USER}" --password-stdin
+  docker pull ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}
+  docker logout
 
 	docker run \
     --env MERKELY_COMMAND=log_deployment \
@@ -24,13 +24,13 @@ merkely_log_deployment()
     --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
     --env MERKELY_FINGERPRINT=$(merkely_fingerprint) \
     --env MERKELY_DESCRIPTION="Deployed to ${environment} in circleci pipeline" \
-    --env MERKELY_ENVIRONMENT="${environment}" \
+    --env MERKELY_ENVIRONMENT="${MERKELY_ENVIRONMENT}" \
     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST="${hostname}" \
+    --env MERKELY_HOST="${MERKELY_HOST}" \
     --rm \
     --volume /var/run/docker.sock:/var/run/docker.sock \
-      ${MERKELY_CHANGE}
+      merkely/change:latest
 }
 
 # - - - - - - - - - - - - - - - - - - -
