9e0f78fe1a9f3b40d2721cdefc1795278d54cec2
Jon Jagger <jon@jaggersoft.com>
2022-08-22 15:15:37 +0100

    CI: add sudo to kosli log deployment script that does apt-get update

--- scripts/kosli_log_deployment.sh
+++ scripts/kosli_log_deployment.sh
@@ -14,12 +14,13 @@ KOSLI_HOST="${2}"
 # brew is not installed on Ubuntu 20.04, so can't do
 # brew install kosli-dev/tap/kosli
 
-apt-get update
-apt-get install --yes wget
+whoami
+sudo apt-get update
+sudo apt-get install --yes wget
 pushd /tmp
-wget https://github.com/kosli-dev/cli/releases/download/v0.1.8/kosli_0.1.8_linux_amd64.tar.gz
-tar -xf kosli_0.1.8_linux_amd64.tar.gz
-mv kosli /usr/local/bin
+sudo wget https://github.com/kosli-dev/cli/releases/download/v0.1.8/kosli_0.1.8_linux_amd64.tar.gz
+sudo tar -xf kosli_0.1.8_linux_amd64.tar.gz
+sudo mv kosli /usr/local/bin
 popd
 
 kosli pipeline deployment report ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG} \
