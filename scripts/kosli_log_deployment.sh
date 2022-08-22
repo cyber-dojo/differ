cb633c3e8a08de47aa4558108eaa8a8b3cda35d2
Jon Jagger <jon@jaggersoft.com>
2022-08-22 15:26:00 +0100

    CI: add missing export prefix when setting up KOSLI env-vars

--- scripts/kosli_log_deployment.sh
+++ scripts/kosli_log_deployment.sh
@@ -1,16 +1,5 @@
 #!/bin/bash -Eeu
 
-VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
-export $(curl "${VERSIONER_URL}/app/.env")
-export CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
-docker pull ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}
-
-KOSLI_OWNER=cyber-dojo
-KOSLI_API_TOKEN=${MERKELY_API_TOKEN}
-KOSLI_PIPELINE=differ
-KOSLI_ENVIRONMENT="${1}"
-KOSLI_HOST="${2}"
-
 # brew is not installed on Ubuntu 20.04, so can't do
 # brew install kosli-dev/tap/kosli
 
@@ -23,9 +12,18 @@ sudo tar -xf kosli_0.1.8_linux_amd64.tar.gz
 sudo mv kosli /usr/local/bin
 popd
 
-env | grep KOSLI
+
+VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
+export $(curl "${VERSIONER_URL}/app/.env")
+export CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
+docker pull ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}
+
+export KOSLI_OWNER=cyber-dojo
+export KOSLI_API_TOKEN=${MERKELY_API_TOKEN}
+export KOSLI_PIPELINE=differ
+export KOSLI_ENVIRONMENT="${1}"
+export KOSLI_HOST="${2}"
 
 kosli pipeline deployment report ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG} \
-  --artifact-type docker \
-  --owner cyber-dojo
+  --artifact-type docker
 
