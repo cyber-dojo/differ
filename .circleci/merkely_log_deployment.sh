40b8511fdd33efc57e17fd17e793ff984da9a036
Jon Jagger <jon@jaggersoft.com>
2022-08-27 09:57:19 +0100

    CI: fix kosli-log-deployment

--- .circleci/merkely_log_deployment.sh
+++ .circleci/merkely_log_deployment.sh
@@ -4,45 +4,45 @@ MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 "${MY_DIR}/../scripts/kosli_log_deployment.sh" "$@"
 
-exit 0
-
-readonly MERKELY_CHANGE=merkely/change:latest
-readonly MERKELY_OWNER=cyber-dojo
-readonly MERKELY_PIPELINE=differ
-
-# - - - - - - - - - - - - - - - - - - -
-kosli_fingerprint()
-{
-  echo "docker://${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}"
-}
-
-# - - - - - - - - - - - - - - - - - - -
-kosli_log_deployment()
-{
-  local -r MERKELY_ENVIRONMENT="${1}"
-  local -r MERKELY_HOST="${2}"
-
-	docker run \
-    --env MERKELY_COMMAND=log_deployment \
-    --env MERKELY_OWNER=${MERKELY_OWNER} \
-    --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
-    --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
-    --env MERKELY_DESCRIPTION="Deployed to ${MERKELY_ENVIRONMENT} in Github Actions pipeline" \
-    --env MERKELY_ENVIRONMENT="${MERKELY_ENVIRONMENT}" \
-    --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
-    --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
-    --env MERKELY_HOST="${MERKELY_HOST}" \
-    --rm \
-    --volume /var/run/docker.sock:/var/run/docker.sock \
-      merkely/change:latest
-}
-
-# - - - - - - - - - - - - - - - - - - -
-VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
-export $(curl "${VERSIONER_URL}/app/.env")
-export CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
-docker pull ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}
-
-readonly ENVIRONMENT="${1}"
-readonly HOSTNAME="${2}"
-kosli_log_deployment "${ENVIRONMENT}" "${HOSTNAME}"
+# exit 0
+#
+# readonly MERKELY_CHANGE=merkely/change:latest
+# readonly MERKELY_OWNER=cyber-dojo
+# readonly MERKELY_PIPELINE=differ
+#
+# # - - - - - - - - - - - - - - - - - - -
+# kosli_fingerprint()
+# {
+#   echo "docker://${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}"
+# }
+#
+# # - - - - - - - - - - - - - - - - - - -
+# kosli_log_deployment()
+# {
+#   local -r MERKELY_ENVIRONMENT="${1}"
+#   local -r MERKELY_HOST="${2}"
+#
+#   docker run \
+#     --env MERKELY_COMMAND=log_deployment \
+#     --env MERKELY_OWNER=${MERKELY_OWNER} \
+#     --env MERKELY_PIPELINE=${MERKELY_PIPELINE} \
+#     --env MERKELY_FINGERPRINT=$(kosli_fingerprint) \
+#     --env MERKELY_DESCRIPTION="Deployed to ${MERKELY_ENVIRONMENT} in Github Actions pipeline" \
+#     --env MERKELY_ENVIRONMENT="${MERKELY_ENVIRONMENT}" \
+#     --env MERKELY_CI_BUILD_URL=${CIRCLE_BUILD_URL} \
+#     --env MERKELY_API_TOKEN=${MERKELY_API_TOKEN} \
+#     --env MERKELY_HOST="${MERKELY_HOST}" \
+#     --rm \
+#     --volume /var/run/docker.sock:/var/run/docker.sock \
+#       merkely/change:latest
+# }
+#
+# # - - - - - - - - - - - - - - - - - - -
+# VERSIONER_URL=https://raw.githubusercontent.com/cyber-dojo/versioner/master
+# export $(curl "${VERSIONER_URL}/app/.env")
+# export CYBER_DOJO_DIFFER_TAG="${CIRCLE_SHA1:0:7}"
+# docker pull ${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}
+#
+# readonly ENVIRONMENT="${1}"
+# readonly HOSTNAME="${2}"
+# kosli_log_deployment "${ENVIRONMENT}" "${HOSTNAME}"
--- scripts/kosli_log_deployment.sh
+++ scripts/kosli_log_deployment.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -Eeu
 
-MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "${MY_DIR}/kosli.sh"
-
 export KOSLI_OWNER=cyber-dojo
 export KOSLI_API_TOKEN=${MERKELY_API_TOKEN}
 export KOSLI_PIPELINE=differ
 export KOSLI_ENVIRONMENT="${1}"
 export KOSLI_HOST="${2}"
+
+MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "${MY_DIR}/kosli.sh"
 
 docker pull $(image_name)
 install_kosli
