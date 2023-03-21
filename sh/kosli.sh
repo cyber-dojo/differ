5e5152d5ce65f2c95ab5a010b8218432d054d067
Jon Jagger <jon@jaggersoft.com>
2023-03-21 21:10:25 +0000

    Use KOSLI_ORG env-var; delete duplicate function

--- sh/kosli.sh
+++ sh/kosli.sh
@@ -3,7 +3,7 @@ set -Eeu
 
 MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-export KOSLI_OWNER=cyber-dojo
+export KOSLI_ORG=cyber-dojo
 export KOSLI_FLOW=differ
 
 export KOSLI_HOST_STAGING=https://staging.app.kosli.com
@@ -98,23 +98,6 @@ kosli_assert_artifact()
   kosli assert artifact "$(tagged_image_name)" \
       --artifact-type docker \
       --host "${hostname}"
-}
-
-# - - - - - - - - - - - - - - - - - - -
-kosli_expect_deployment()
-{
-  local -r environment="${1}"
-  local -r hostname="${2}"
-
-  # In .github/workflows/main.yml deployment is its own job
-  # and the image must be present to get its sha256 fingerprint.
-  docker pull "$(tagged_image_name)"
-
-  kosli expect deployment "$(tagged_image_name)" \
-    --artifact-type docker \
-    --description "Deployed to ${environment} in Github Actions pipeline" \
-    --environment "${environment}" \
-    --host "${hostname}"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - -
