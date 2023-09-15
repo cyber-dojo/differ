4ea1f38d907613408c4796e9da8abb9c88f47d3f
JonJagger <jon@kosli.com>
2023-09-15 06:20:32 +0100

    CI: try upgrading do ubuntu 22.04 and not upgrading docker-compose

--- build_test_publish.sh
+++ build_test_publish.sh
@@ -29,7 +29,7 @@ export $(echo_versioner_env_vars)
 exit_zero_if_show_help "$@"
 exit_non_zero_unless_installed docker
 exit_non_zero_unless_installed docker-compose
-on_ci_upgrade_docker_compose
+# on_ci_upgrade_docker_compose
 remove_old_images
 #generate_env_var_yml_files
 on_ci_kosli_create_flow
