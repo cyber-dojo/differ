af5b45de2a746d4b8f3f033cabdce1224b572896
JonJagger <jon@kosli.com>
2023-09-15 06:15:16 +0100

    CI: try installing docker-compose with sudo

--- sh/on_ci_upgrade_docker_compose.sh
+++ sh/on_ci_upgrade_docker_compose.sh
@@ -8,7 +8,7 @@ on_ci_upgrade_docker_compose()
   else
     echo 'on CI so upgrading docker-compose'
     # https://stackoverflow.com/questions/49839028
-    curl -L "https://github.com/docker/compose/releases/download/`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | sed 's#.*tag/##g' && echo`/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/`curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/docker/compose/releases/latest | sed 's#.*tag/##g' && echo`/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose
   fi
   echo
 }
