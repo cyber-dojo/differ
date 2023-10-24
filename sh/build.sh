581ebf0bc2a56ddb1aed614483bf6ea879d4b6d3
JonJagger <jon@kosli.com>
2023-10-24 12:07:22 +0100

    CI: prepare sh/build.sh script [ci skip]

---
+++ sh/build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -Eeu
+
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+export SH_DIR="${ROOT_DIR}/sh"
+
+source "${SH_DIR}/exit_non_zero_unless_installed.sh"
+source "${SH_DIR}/exit_zero_if_show_help.sh"
+source "${SH_DIR}/on_ci_upgrade_docker_compose.sh"
+source "${SH_DIR}/remove_old_images.sh"
+source "${SH_DIR}/build_tagged_images.sh"
+source "${SH_DIR}/tag_images_to_latest.sh"
+source "${SH_DIR}/echo_versioner_env_vars.sh"
+export $(echo_versioner_env_vars)
+
+exit_zero_if_show_help "$@"
+exit_non_zero_unless_installed docker
+exit_non_zero_unless_installed docker-compose
+on_ci_upgrade_docker_compose
+remove_old_images
+build_tagged_images "$@"
+tag_images_to_latest "$@"
--- sh/build_tagged_images.sh
+++ sh/build_tagged_images.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+set -Eeu
+
+export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+export SH_DIR="${ROOT_DIR}/sh"
 
 source "${SH_DIR}/augmented_docker_compose.sh"
 
