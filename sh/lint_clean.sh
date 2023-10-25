149220584a1733031e582f8680e227abc08bcd69
JonJagger <jon@kosli.com>
2023-10-25 11:05:01 +0100

    CI: fix lint calls [ci skip]

--- sh/lint_clean.sh
+++ sh/lint_clean.sh
@@ -5,10 +5,14 @@ export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 export SH_DIR="${ROOT_DIR}/sh"
 source "${SH_DIR}/lib.sh"
 
-sudo gem install rubocop --no-document
-local -r LINT_EVIDENCE_DIR=/tmp/evidence/lint
-mkdir -p "${LINT_EVIDENCE_DIR}"
-cp "$(repo_root)/.rubocop.yml" "${LINT_EVIDENCE_DIR}"/.rubocop.yml
-ls -al "${LINT_EVIDENCE_DIR}"
-set +e
-rubocop . | tee "${LINT_EVIDENCE_DIR}"/rubocop.log
+lint_clean()
+{
+  sudo gem install rubocop --no-document
+  local -r LINT_EVIDENCE_DIR=/tmp/evidence/lint
+  mkdir -p "${LINT_EVIDENCE_DIR}"
+  cp "$(repo_root)/.rubocop.yml" "${LINT_EVIDENCE_DIR}"/.rubocop.yml
+  ls -al "${LINT_EVIDENCE_DIR}"
+  set +e
+  rubocop . | tee "${LINT_EVIDENCE_DIR}"/rubocop.log
+}
+export -f lint_clean
