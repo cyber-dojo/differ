d45c2a50165d12be735f77607cb97443f1269d91
JonJagger <jon@kosli.com>
2023-10-25 11:36:49 +0100

    CI: rubocop debugging [ci skip]

--- sh/lint_clean.sh
+++ sh/lint_clean.sh
@@ -1,18 +1,17 @@
 #!/usr/bin/env bash
 set -Eeu
 
-export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-export SH_DIR="${ROOT_DIR}/sh"
-source "${SH_DIR}/lib.sh"
+repo_root()
+{
+  git rev-parse --show-toplevel
+}
 
 lint_clean()
 {
-  set -x
-  sudo gem install rubocop --no-document
   local -r LINT_EVIDENCE_DIR=/tmp/evidence/lint
   mkdir -p "${LINT_EVIDENCE_DIR}"
   cp "$(repo_root)/.rubocop.yml" "${LINT_EVIDENCE_DIR}"/.rubocop.yml
   ls -al "${LINT_EVIDENCE_DIR}"
-  rubocop . | tee "${LINT_EVIDENCE_DIR}"/rubocop.log
+  rubocop "$(repo_root)" | tee "${LINT_EVIDENCE_DIR}"/rubocop.log
 }
 export -f lint_clean
