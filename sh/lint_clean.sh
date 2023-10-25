cc4b54004209f00ee048161c1d33a8c448fc84c0
JonJagger <jon@kosli.com>
2023-10-25 11:20:38 +0100

    CI: add set-x debug to lint script [ci skip]

--- sh/lint_clean.sh
+++ sh/lint_clean.sh
@@ -7,12 +7,12 @@ source "${SH_DIR}/lib.sh"
 
 lint_clean()
 {
+  set -x
   sudo gem install rubocop --no-document
   local -r LINT_EVIDENCE_DIR=/tmp/evidence/lint
   mkdir -p "${LINT_EVIDENCE_DIR}"
   cp "$(repo_root)/.rubocop.yml" "${LINT_EVIDENCE_DIR}"/.rubocop.yml
   ls -al "${LINT_EVIDENCE_DIR}"
-  set +e
   rubocop . | tee "${LINT_EVIDENCE_DIR}"/rubocop.log
 }
 export -f lint_clean
