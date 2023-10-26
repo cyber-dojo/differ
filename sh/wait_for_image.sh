9bdeeaa9767c8d411c6a02ce8ef666340436ae01
JonJagger <jon@kosli.com>
2023-10-26 16:00:14 +0100

    CI: add correct needs: to staging_main.yml [ci skip]

--- sh/wait_for_image.sh
+++ sh/wait_for_image.sh
@@ -4,4 +4,6 @@ set -Eeu
 export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 export SH_DIR="${ROOT_DIR}/sh"
 
-# wait for image to be pulled from dockerhub for 5 mins, then give up with error
+# TODO: wait for image to be pulled from dockerhub in repeating probe
+#       for maximum of 5 mins, then give up with error
+sleep 120
