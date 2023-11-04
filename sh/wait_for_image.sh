312325a013f24503e6811270a6b33e83bd5f738a
JonJagger <jon@kosli.com>
2023-11-04 15:07:58 +0000

    Fix sh/wait_for_image.sh

--- sh/wait_for_image.sh
+++ sh/wait_for_image.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
-set -Eeu
+set -Eu
 
 TAG="$(echo ${GITHUB_SHA} | head -c7)"
-MAX_TRIES=30  # every 10s for 5 mins
-COUNTER=0
+MAX_ATTEMPTS=30  # every 10s for 5 minuntes
+ATTEMPTS=0
+
 until docker pull cyberdojo/differ:${TAG}
 do
   sleep 10
-  [[ ${COUNTER} -eq ${MAX_TRIES} ]] && echo "Failed!" && exit 1
-  echo "Trying docker pull cyberdojo/differ:${TAG} again. Try #${COUNTER}"
-  ((COUNTER++))
+  [[ ${ATTEMPTS} -eq ${MAX_ATTEMPTS} ]] && echo "Failed!" && exit 1
+  echo "Trying docker pull cyberdojo/differ:${TAG} again. Attempt #${ATTEMPTS}"
+  ((ATTEMPTS++))
 done
