6f7179e9436b22676c0964aeccea2bcaf0e8f170
JonJagger <jon@kosli.com>
2023-11-06 20:05:39 +0000

    Fix typo in script comment [ci skip]

--- sh/wait_for_image.sh
+++ sh/wait_for_image.sh
@@ -2,13 +2,13 @@
 set -Eu
 
 TAG="$(echo ${GITHUB_SHA} | head -c7)"
-MAX_ATTEMPTS=30  # every 10s for 5 minuntes
-ATTEMPTS=0
+MAX_ATTEMPTS=30  # every 10s for 5 minutes
+ATTEMPTS=1
 
 until docker pull cyberdojo/differ:${TAG}
 do
   sleep 10
   [[ ${ATTEMPTS} -eq ${MAX_ATTEMPTS} ]] && echo "Failed!" && exit 1
-  echo "Trying docker pull cyberdojo/differ:${TAG} again. Attempt #${ATTEMPTS}"
   ((ATTEMPTS++))
+  echo "Trying docker pull cyberdojo/differ:${TAG} again. Attempt #${ATTEMPTS}"
 done
