6bc82d7d512f97c2a6799d9b6c564644597707fe
Jon Jagger <jon@kosli.com>
2023-12-02 12:10:22 +0000

    CI: stay in same dir in copy_in_saver_test_data function (#15)

--- sh/copy_in_saver_test_data.sh
+++ sh/copy_in_saver_test_data.sh
@@ -6,7 +6,8 @@ copy_in_saver_test_data()
   local -r SAVER_CID=$(docker ps --filter status=running --format '{{.Names}}' | grep "saver")
   local -r DEST_PATH=/cyber-dojo
   # You cannot docker cp to a tmpfs, so tar-piping instead...
-  cd ${SRC_PATH} \
-    && tar -c . \
-    | docker exec -i ${SAVER_CID} tar x -C ${DEST_PATH}
+  pushd "${SRC_PATH}" || exit 99
+  tar -c . \
+    | docker exec -i "${SAVER_CID}" tar x -C ${DEST_PATH}
+  popd || exit 99
 }
