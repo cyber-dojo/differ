5ef1680b8afb385286d13874eb7c5a0c4e9db831
Jon Jagger <jon@kosli.com>
2024-01-09 09:46:47 +0000

    CI: fix wait-for-image script (#50)

--- sh/wait_for_image.sh
+++ sh/wait_for_image.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 set -Eu
 
-GIT_COMMIT="${1}"                         # eg 756c7286adbcca25ec7a1e098e43758b270ebed6
-TAG="$(echo "${GIT_COMMIT}" | head -c7)"  # eg 756c728
-IMAGE_NAME="cyberdojo/differ:${TAG}"      # eg cyberdojo/differ:756c728
+# See https://gitlab.com/cyber-dojo/creator/-/blob/main/.gitlab/workflows/dev-readme.md
 
-MAX_WAIT_TIME=$((5 * 60))  # 5 minutes
-SLEEP_TIME=10              # 10 secs
-MAX_ATTEMPTS=$(( MAX_WAIT_TIME / SLEEP_TIME ))
+readonly IMAGE_NAME="${1}"  # eg cyberdojo/differ:756c728
+readonly MAX_WAIT_TIME=5    # max time to wait for IMAGE_NAME to be pushed, in minutes
+readonly SLEEP_TIME=10      # wait time between pull checks, in seconds
+readonly MAX_ATTEMPTS=$(( MAX_WAIT_TIME * 60 / SLEEP_TIME ))
+
 ATTEMPTS=1
 
 until docker pull "${IMAGE_NAME}"
 do
-  sleep 10
+  sleep ${SLEEP_TIME}
   [[ ${ATTEMPTS} -eq ${MAX_ATTEMPTS} ]] && echo "Failed!" && exit 1
   ((ATTEMPTS++))
-  echo "Trying docker pull cyberdojo/differ:${TAG} again. Attempt #${ATTEMPTS}"
+  echo "Waiting for ${IMAGE_NAME} to be pushed to its registry"
+  echo "Attempt # ${ATTEMPTS} / ${MAX_ATTEMPTS}"
 done
+echo "Success: Artifact ${IMAGE_NAME} has been pushed to its registry"
+exit 0
