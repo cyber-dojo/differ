6218c45aa3f5f16b8f5c8186c8b8c904f2650d07
JonJagger <jon@kosli.com>
2024-02-03 09:11:20 +0000

    Don't rebuild image before running tests on CI

--- sh/build_image.sh
+++ sh/build_image.sh
@@ -8,18 +8,22 @@ source "${SH_DIR}/check_embedded_sha_env_var.sh"
 source "${SH_DIR}/echo_env_vars.sh"
 source "${SH_DIR}/exit_non_zero_unless_installed.sh"
 source "${SH_DIR}/lib.sh"
-source "${SH_DIR}/on_ci_upgrade_docker_compose.sh"
 source "${SH_DIR}/remove_old_images.sh"
 source "${SH_DIR}/tag_images_to_latest.sh"
 
 source "${SH_DIR}/echo_versioner_env_vars.sh"
 export $(echo_versioner_env_vars)
 
-exit_non_zero_unless_installed docker
-exit_non_zero_unless_installed docker-compose
-on_ci_upgrade_docker_compose
-remove_old_images
-build_tagged_images "$@"
-tag_images_to_latest "$@"
-check_embedded_sha_env_var
-echo_env_vars
+if on_ci ; then
+  echo On CI so not re-building the image
+  echo Instead, letting docker pull the built image
+else
+  echo Not on CI so building the image
+  exit_non_zero_unless_installed docker
+  exit_non_zero_unless_installed docker-compose
+  remove_old_images
+  build_tagged_images "$@"
+  tag_images_to_latest "$@"
+  check_embedded_sha_env_var
+  echo_env_vars
+fi
