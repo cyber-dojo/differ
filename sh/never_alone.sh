fbd04da191cee0abb669b69cb928574584d81e12
JonJagger <jon@kosli.com>
2024-07-30 16:22:49 +0100

    Minor tidying

--- sh/never_alone.sh
+++ sh/never_alone.sh
@@ -1,44 +1,46 @@
 #!/usr/bin/env bash
 set -Eeu
 
+KOSLI_ORG="${KOSLI_ORG:-cyber-dojo}"                       # wip default
+KOSLI_API_TOKEN="${KOSLI_API_TOKEN:-80rtyg24o0fgh0we8fh}"  # wip default=fake read-only token
+KOSLI_ENVIRONMENT="${KOSLI_ENVIRONMENT:-aws-prod}"         # wip default
 
-KOSLI_ENVIRONMENT=aws-prod
-
-function get_previous {
-    local -r snapshot_json_filename=snapshot.json
+function get_current
+{
+    local -r snapshot_json_filename=/tmp/snapshot.json
 
     # Use Kosli CLI to get info on what artifacts are currently running in the given environment
-    # (docs/snapshot.json contains an example json file)
-    kosli get snapshot "${KOSLI_ENVIRONMENT}" \
-      --org="cyber-dojo" \
-      --api-token="80rtyg24o0fgh0we8fh" \
-      --output=json > "${snapshot_json_filename}" 
+    kosli get snapshot "${KOSLI_ENVIRONMENT}" --output=json > "${snapshot_json_filename}"
 
     artifacts_length=$(jq '.artifacts | length' ${snapshot_json_filename})
-    for i in $(seq 0 $(( ${artifacts_length} - 1 )))
+    for i in $(seq 0 $(( artifacts_length - 1 )))
     do
         annotation_type=$(jq -r ".artifacts[$i].annotation.type" ${snapshot_json_filename})
         if [ "${annotation_type}" != "exited" ] ; then
           flow=$(jq -r ".artifacts[$i].flow_name" ${snapshot_json_filename})
           if [ "${flow}" == "differ-ci" ] ; then
             git_commit=$(jq -r ".artifacts[$i].git_commit" ${snapshot_json_filename})
-            echo $git_commit
+            echo "${git_commit}"
             return
           fi
         fi
     done
 }
 
-current=a96283918944a53b70175ce9605984f4e9e05630
-previous=f4215fc5060e6e7c60b32be05b657929a271efcc
-#previous=$(get_previous)
-#echo $previous
+# proposed: the commit corresponding to the Trail for the live workflow, which is building an Artifact to be deployed
+# current: the commit corresponding to the Artifact currently running in KOSLI_ENVIRONMENT, that will be replaced
+proposed=$(git rev-parse main)
+#current=$(get_current)
+current=f4215fc5060e6e7c60b32be05b657929a271efcc   # wip (2 deploys back, because on main, proposed==currently)
 
-declare -A commits=$(git rev-list --first-parent ${previous}..${current})
+# shellcheck disable=SC2155
+declare -A commits=$(git rev-list --first-parent "${current}..${proposed}")
 
-for commit in ${commits[@]}; do
-  echo $commit
+for commit in "${commits[@]}"; do
+  echo "${commit}"
+  # TODO: get author + committer info for each
 done
+
 
 
 #gh pr list --search "a96283918944a53b70175ce9605984f4e9e05630" --state merged --json assignees --json author --json mergedBy --json state
@@ -49,7 +51,6 @@ done
 # function pr_for_sha {
 #   git log --merges --ancestry-path --oneline ${x}..main
 # }
-
 # pr_for_sha
 
 #| grep 'pull request' | tail -n1 | awk '{print $5}' | cut -c2- | xargs -I % open https://github.com/$GITHUB_UPSTREAM/${PWD##*/}/pull/%
