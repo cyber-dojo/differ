d099c4f343ab30ebdfde82020a46b611ad46685e
Tore Martin Hagen <tore@kosli.com>
2024-08-08 08:21:18 +0200

    Never alone: script can now get commit and pr

--- sh/never_alone_get_commit_and_pull_request_info.sh
+++ sh/never_alone_get_commit_and_pull_request_info.sh
@@ -63,30 +63,13 @@ function get_commit_and_pull_request
     local commit_sha=$1; shift
     local result_file=$1; shift
 
-    # Use gh instead of git so we can keep the commit depth of 1. The order of the response for gh is reversed
-    # so I do a tac at the end to get it the same order.
-#    list_separator=""
-#    echo "{" > ${result_file}
-
-    commit_data=$(gh search commits --hash "${commit_sha}" --json author,sha)
+    commit_data=$(gh search commits --hash "${commit_sha}" --json author)
     pr_data=$(gh pr list --search "${commit_sha}" --state merged --json author,latestReviews,mergeCommit,mergedAt,url)
 
-#    if [ "$pr_data" = "[]" ]; then
-#        # Commit is not merged back to master (this will happen if you run this on a branch)
-#        echo '{"sha": "'${commit_sha}'"}' >> "${result_file}"
-#    elif [ "$(echo "$pr_data" | jq '. | length')" -eq 0 ]; then
-#        # No pull request found for that commit, so do a new request to get the commit
-#        commit_data=$(gh search commits --hash "${commit_sha}" --json author,sha)
-#        echo "$commit_data" | jq '.[0]' >> "${result_file}"
-#    else
-#        # The PR data does not contain the commit sha so we add it manually. Use 'sha' as key since that is
-#        # what is used in the 'gh search commits' command
-#        echo "$pr_data" | jq '{pullrequest: .[0]}' >> "${result_file}"
-#    fi
+    combined_data=$(jq -n --arg commitsha "$commit_sha" --argjson commit "$commit_data" --argjson pr "$pr_data" \
+      '{commit_sha: $commitsha, commit: $commit[0], pull_request: $pr[0]}')
 
-    combined_data=$(jq -n --argjson commit "$commit_data" --argjson pr "$pr_data" '{commit: $commit[0], pullrequest: $pr[0]}')
     echo "${combined_data}" >> ${result_file}
-
 }
 
 
