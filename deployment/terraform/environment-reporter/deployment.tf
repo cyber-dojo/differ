38f3dc8b63abb632ac94a12b3f818b49f8047fa1
Artem Zhelezov <36639304+zhelezovartem@users.noreply.github.com>
2024-08-27 19:24:23 +0500

    kosli-reporter: update kosli cli version

--- deployment/terraform/environment-reporter/deployment.tf
+++ deployment/terraform/environment-reporter/deployment.tf
@@ -5,15 +5,13 @@ locals {
 }
 
 module "lambda_reporter" {
-  for_each = var.kosli_hosts
-
   source  = "kosli-dev/kosli-reporter/aws"
   version = "0.5.0"
 
-  name                              = "differ-state-reporter-${each.key}"
+  name                              = "differ-state-reporter"
   kosli_environment_type            = "s3"
-  kosli_host                        = each.value
-  kosli_cli_version                 = "v2.7.8"
+  kosli_host                        = var.KOSLI_HOST
+  kosli_cli_version                 = "v2.10.13"
   kosli_environment_name            = "terraform-state-differ-${var.env}"
   kosli_org                         = "cyber-dojo"
   reported_aws_resource_name        = local.state_bucket_name
--- deployment/terraform/environment-reporter/variables.tf
+++ deployment/terraform/environment-reporter/variables.tf
@@ -1,9 +1,5 @@
-variable "kosli_hosts" {
-  type = map(any)
-  default = {
-    staging = "https://staging.app.kosli.com"
-    prod    = "https://app.kosli.com"
-  }
+variable "KOSLI_HOST" {
+  type = string
 }
 
 variable "env" {
