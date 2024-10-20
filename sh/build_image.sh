cd39886e0d02ae5ca9aa536e6ebe8c1091ce6993
JonJagger <jon@kosli.com>
2024-10-20 15:12:13 +0100

    Ensure client image is built in CI workflow

--- sh/build_image.sh
+++ sh/build_image.sh
@@ -10,28 +10,35 @@ source "${ROOT_DIR}/sh/lib.sh"
 
 build_image()
 {
-  if [ -n "${CI:-}" ]; then
-    echo In CI workflow - using image built with the GitHub Action
+  local -r target="${1}"
+
+  if [ -n "${CI:-}" ] && [ "${target}" == 'server' ] ; then
+    echo In CI workflow - using server image built with the GitHub Action
   else
     echo Not in CI workflow - building image
     exit_non_zero_unless_installed docker
     containers_down
     export $(echo_versioner_env_vars)
     #remove_old_images
-    build_tagged_images "$@"
+    #build_tagged_images "$@"
+    docker compose build --build-arg COMMIT_SHA="${COMMIT_SHA}" server
+    if [ "${target}" != 'server' ]; then
+      docker compose build --build-arg COMMIT_SHA="${COMMIT_SHA}" client
+    fi
+
     #tag_images_to_latest "$@"
     #check_embedded_sha_env_var
     #echo_env_vars
   fi
 }
 
-build_tagged_images()
-{
-  local -r target="${1}"
-  docker compose build --build-arg COMMIT_SHA="${COMMIT_SHA}" server
-  if [ "${target}" != 'server' ]; then
-    docker compose build --build-arg COMMIT_SHA="${COMMIT_SHA}" client
-  fi
-}
+#build_tagged_images()
+#{
+#  local -r target="${1}"
+#  docker compose build --build-arg COMMIT_SHA="${COMMIT_SHA}" server
+#  if [ "${target}" != 'server' ]; then
+#    docker compose build --build-arg COMMIT_SHA="${COMMIT_SHA}" client
+#  fi
+#}
 
 build_image "$@"
