56c04a24b159788065adbe52f5cfd1f82752d35f
JonJagger <jon@kosli.com>
2024-10-20 18:54:39 +0100

    Fix demo.sh

--- sh/demo.sh
+++ sh/demo.sh
@@ -5,19 +5,13 @@ export ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 source "${ROOT_DIR}/sh/lib.sh"
 
-demo()
-{
-  local -r TMP_HTML_FILENAME=/tmp/differ-demo.html
-  docker exec \
-    "${CYBER_DOJO_DIFFER_CLIENT_CONTAINER_NAME}" \
-      sh -c 'ruby /differ/app/html_demo.rb' \
-        > ${TMP_HTML_FILENAME}
-  open "file://${TMP_HTML_FILENAME}"
-}
-
 exit_non_zero_unless_installed docker
 export $(echo_versioner_env_vars)
-server_up_healthy_and_clean
-client_up_healthy_and_clean "$@"
+docker compose up --wait --wait-timeout=10 client
 copy_in_saver_test_data
-demo
+TMP_HTML_FILENAME=/tmp/differ-demo.html
+docker exec \
+  "${CYBER_DOJO_DIFFER_CLIENT_CONTAINER_NAME}" \
+    sh -c 'ruby /differ/html_demo.rb' \
+      > ${TMP_HTML_FILENAME}
+open "file://${TMP_HTML_FILENAME}"
