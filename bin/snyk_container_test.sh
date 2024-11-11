e3b597aa46f080356285eb1095ca6af2da5b6f40
JonJagger <jon@kosli.com>
2024-11-11 11:05:02 +0000

    Report rubocop results in junit format

--- bin/snyk_container_test.sh
+++ bin/snyk_container_test.sh
@@ -7,8 +7,10 @@ source "${ROOT_DIR}/bin/lib.sh"
 
 exit_non_zero_unless_installed snyk
 export $(echo_versioner_env_vars)
-readonly image_name="cyberdojo/differ:${CYBER_DOJO_DIFFER_TAG}"
-snyk container test "${image_name}" \
+readonly IMAGE_NAME="${CYBER_DOJO_DIFFER_IMAGE}:${CYBER_DOJO_DIFFER_TAG}"
+
+snyk container test "${IMAGE_NAME}" \
       --file="${ROOT_DIR}/Dockerfile" \
       --json-file-output="${ROOT_DIR}/snyk.container.scan.json" \
       --policy-path="${ROOT_DIR}/.snyk"
+
