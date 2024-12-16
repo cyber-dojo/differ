d3809df7075d39a463b5117090fa20366c5edafa
Steve Tooke <steve@took.es>
2024-12-16 11:55:53 +0000

    make custom attestation (#270)
    * Use API to make custom attestation
    
    * Flow should be org
    
    Also be louder with curl

--- bin/loud_curl_form_data.sh
+++ bin/loud_curl_form_data.sh
@@ -10,19 +10,24 @@ loud_curl_form_data()
   local -r URL="${2}"
   local -r API_TOKEN="${3}"
   local -r JSON_PAYLOAD="${4}"
+  local -r OUTPUT_FILE=$(mktemp)
 
   # Build curl form arguments
   CURL_FORM_ARGS=("--form" "data_json=${JSON_PAYLOAD}")
 
   HTTP_CODE=$(curl --header 'Content-Type: multipart/form-data' \
        --user "${API_TOKEN}:unused" \
+       --output "${OUTPUT_FILE}" \
        --write-out "%{http_code}" \
+       --silent \
        --request "${METHOD}" \
        "${CURL_FORM_ARGS[@]}" \
        ${URL})
   set -e
   echo -n .
   if [[ ${HTTP_CODE} -lt 200 || ${HTTP_CODE} -gt 299 ]] ; then
+      >&2 cat "${OUTPUT_FILE}"
+      rm "${OUTPUT_FILE}"
       exit 22
   fi
 }
